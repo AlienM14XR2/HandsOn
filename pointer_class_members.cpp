aa55d0d256d6ac14342fc0a3568e4dc8c062d622
alienm14xr2 <alienm14xr2@gmail.com>
2023-03-13 09:41:37 +0900

    ストラテジーにおけるポリモーフィズムの実現と実践。

--- pointer_class_members.cpp
+++ pointer_class_members.cpp
@@ -106,6 +106,23 @@ public:
      * t1 < t2 , return 1.
      * t1 == t2 , return 0.
      * t1 > t2, return -1.
+     *
+     * これ、virtual をつけてないんだけど、各派生クラスのオブジェクト
+     * は override した、自分の関数が使われている。今回、virtual を
+     * つけなかった理由は最後に述べる。
+     *  
+     * 基底クラスの参照もしくはポインタ経由で操作する場合、メンバ関数が 
+     * virtual じゃないと実際のオブジェクトの型に関わらず、
+     * 基底クラスのメンバ関数を呼び出してしまう。つまり、このままでは
+     * ポリモーフィズム（多態性）を実現しているとは言えない。
+     * 今回は、Strategy をインタフェースとして利用しなかったため
+     * 問題にはならなかっただけ。
+     * 
+     * virtual にしなかったのは、t1 < t2, t1 == t2 がコンパイラに叱らないのか
+     * 確認したかったから。C++ではoperator演算子のoverrideが必要なはずだから。
+     * 今回は template も利用している。その複合的な結果が知りたかった。
+     * この場合、コンパイルはパスしてもポリモーフィズムを利用した時に
+     * 実行時エラーになるはず。
     */
     int compare(T t1, T t2) {
         cout << "Yeah I'm Strategy method." << endl;
@@ -117,8 +134,24 @@ public:
             return -1;
         }
     }
+    /**
+     * t1とt2を比較してね（この場合はお願いになる。
+     * 
+     * t1 < t2 , return 1.
+     * t1 == t2 , return 0.
+     * t1 > t2, return -1.
+     * 
+     * ポリモーフィズムを実現したかったらこっちを使う。
+     * pure virtual にしたかったけど、コンパイラに叱られた。
+     * おそらく、宣言だけで、定義が見当たらなかったからだろう。
+     * 分かったから試してみる。
+     * 
+    */
+    virtual int pureVirtualCompare(const T& t1, const T& t2) const=0;
+
     // 継承前提なので、仮想関数として一応、デストラクタは用意した。
-    ~Strategy(){cout << "Debug. == Strategy Destructor." << endl;}
+    // コメントで言っていながら、virtualつけてないとは。
+    virtual ~Strategy(){cout << "Debug. == Strategy Destructor." << endl;}
 };
 
 /**
@@ -136,6 +169,16 @@ public:
             return -1;
         }
     }
+    int pureVirtualCompare(const Person& p1, const Person& p2) const override {
+        if(p1.power < p2.power) {
+            return 1;
+        } else if( p1.power == p2.power ) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
 };
 
 /**
@@ -152,6 +195,16 @@ public:
             return -1;
         }
     }
+    int pureVirtualCompare(const Person& p1, const Person& p2) const override {
+        if(p1.intelligent < p2.intelligent) {
+            return 1;
+        } else if(p1.intelligent == p2.intelligent) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
 };
 /**
  * こんな風にコードの中に埋没、溶け込みすぎるロジックを
@@ -200,10 +253,55 @@ void test_Intelligent_Compare() {
     cout << "ret is " << ret << endl;
 }
 
+/**
+ * うん、これも脇道で今回のクラスメンバ関数のポインタとは無関係だが
+ * ポリモーフィズムの確認がしたかった。
+ * 勉強になった、C++は面倒くさいと、Javaは楽チンだったのね。
+ * 
+ * - 基底クラスのポリモーフィズム実現メソッドには必ず virtual をつける。
+ * e.g.
+ * virtual int virtualCompare(const T& t1, const T& t2) const
+ * 
+ * - 基底クラスのポリモーフィズム実現メソッドには必ず参照修飾子をつける。
+ *   次の例でいうと最後にある const が参照修飾子と呼ばれるもの。
+ * e.g.
+ * virtual int virtualCompare(const T& t1, const T& t2) const
+ * 
+ * - 派生クラスでは、ポリモーフィズム実現メソッドには必ず参照修飾子をつける。
+ *   次の例でいうと const override
+ * e.g.
+ * int virtualCompare(const Person& p1, const Person& p2) const override
+ * 
+ * - インタフェースとして基底クラスの型で、派生オブジェクトを作る。
+ * e.g.
+ * const Strategy<Person>& iface = PowerComparator();
+ * 
+ * 感想。
+ * レシピを知ってることと、その料理が作れることは別だということだね。
+ * オレは料理研究家や低俗な批評家になりたいんじゃなく、料理人になりたい。
+ * まだ、駆出しか。
+ * 
+*/
+void executor() {
+    cout << "----------------------- executor " << endl;
+    Person nancy = Person::toPerson("Nancy",2,512,2);
+    Person bob = Person::toPerson("bob",99,9,99);
+    const Strategy<Person>& iface = PowerComparator();
+    int ret = iface.pureVirtualCompare(nancy,bob);
+    cout << "ret is " << ret << endl;
+}
+
+void executePolymorphism(const Strategy<Person>& iface, const Person& p1, const Person& p2) {
+    cout << "----------------------- executePolymorphism " << endl;
+    int ret = iface.pureVirtualCompare(p1,p2);
+    cout << "ret is " << ret << endl;
+}
+
 int main() {
     cout << "START クラスメンバに対するポインタ ========== " << endl;
     test_Power_Compare();
     test_Intelligent_Compare();
+
     //
     // ここから、本題のクラスのメンバ関数とメンバ変数のポインタの実践を行う。
     // 中々ここまで、辿り着かなんだが、それが必要なのだよ、今の私には。
@@ -224,8 +322,23 @@ int main() {
     // 型に厳しいC++、次のものはいけるのか
     // うん、現状のオレの知識では、ダメなことなのか、単なる構文エラーなのかが分からない。
     // リファレンス先生もここまでは言及していない。
-    // 今は保留しておく。
+    // 今は保留としておく。
 //    int (Strategy::*sc)(T t1, T t2) = &Strategy::compare;
+
+    executor();
+
+    Person foo = Person::toPerson("Foo",5,100,250);
+    Person bar = Person::toPerson("Bar",10,100,50);
+    const Strategy<Person>& iface = PowerComparator();
+    executePolymorphism(iface,foo,bar);
+    const Strategy<Person>& iface2 = IntelligentComparator();
+    executePolymorphism(iface2,foo,bar);
+    // うん、楽しい。
+    // こんな感じでリファレンスと,その実践を繰り返していきたいね。
+    // リファレンスは読み易いけど、サンプルだけではどうしても足りない。
+    // それを今後もGoFを的にして続けていくよ。
+    // もう、このファイル、ストラテジーパターンでは限界が来た。
+    // 次の適当な的をみつけよう。
 
 /**
  * // 構造体のメンバ関数のポインタ 
