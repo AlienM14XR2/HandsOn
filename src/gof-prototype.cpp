a7ce42952846321a5e0167628d0d8b5952910305
alienm14xr2 <alienm14xr2@gmail.com>
2023-04-19 20:25:27 +0900

    最後は結局 Gof Prototype とはなんの脈絡もないスレッドの勉強になった。 - まぁ、そんなことは気にしない、IT筋トレ中だから：）

--- src/gof-prototype.cpp
+++ src/gof-prototype.cpp
@@ -7,6 +7,7 @@
 #include <cassert>
 #include <thread>
 #include <future>
+#include <vector>
 
 using namespace std;
 
@@ -201,6 +202,114 @@ int test_Thread_Prototype() {
     return 0;
 }
 
+/**
+  折角だから、Prototype とどれ程の関係性を持たせられるか、甚だ疑問ではあるが、現在、自分が考える
+  最高の形のスレッド利用を実現してみたい。力試しかな。
+
+  - スレッド内の処理は各々、別、同期はしない。ただし、メインスレッドとの同期は行う。（join.
+  - Vector コンテナで矢継ぎ早にスレッドは起動させる。@see thread_02.cpp
+  - クラスで管理できるものはクラス内に隠蔽する。thread future promise
+  - worker とその内部関数、便宜的に worker’s method は複数用意する。
+
+  現状の自分の知識と技術で実現できるのはこの辺りだろう。
+  おおまかな、仕様は決まった。では早速実装してみる。 
+*/
+int compute_a(const int& version) {
+    ptr_lambda_debug<const string&,const int&>("compute_a ... ",version);
+    return 0;
+}
+int compute_b(const int& version) {
+    ptr_lambda_debug<const string&,const int&>("compute_b ... ",version);
+    return 0;
+}
+int compute_c(const int& version) {
+    ptr_lambda_debug<const string&,const int&>("compute_c ... ",version);
+    return 0;
+}
+void worker_a(promise<int> promise_, int version) {
+    try {
+        promise_.set_value(compute_a(version));
+    } catch(exception& e) {
+        promise_.set_exception(current_exception());
+    }
+}
+void worker_b(promise<int> promise_, int version) {
+    try {
+        promise_.set_value(compute_b(version));
+    } catch(exception& e) {
+        promise_.set_exception(current_exception());
+    }
+}
+void worker_c(promise<int> promise_, int version) {
+    try {
+        promise_.set_value(compute_c(version));
+    } catch(exception& e) {
+        promise_.set_exception(current_exception());
+    }
+}
+
+class SandboxThread final : public virtual Prototype<SandboxThread*> {
+    static const int THREAD_SIZE = 3;
+public:
+    SandboxThread(){
+    }
+    SandboxThread(const SandboxThread& own) {
+    }
+    ~SandboxThread() {}
+    void doThread_A_B_C() {
+        try {
+            promise<int> promise_a, promise_b, promise_c;
+            future<int> future_a = promise_a.get_future();
+            future<int> future_b = promise_b.get_future();
+            future<int> future_c = promise_c.get_future();
+            vector<thread> threads(THREAD_SIZE);
+            int i = 0;
+            for(thread& t: threads) {
+                switch(i) {
+                    case 0: 
+                        t = thread{worker_a,move(promise_a),move(i+1)};
+                        future_a.get();
+                        break;
+                    case 1: 
+                        t = thread{worker_b,move(promise_b),move(i+1)};
+                        future_b.get();
+                        break;
+                    case 2: 
+                        t = thread{worker_c,move(promise_c),move(i+1)};
+                        future_c.get();
+                        break;
+                }
+                i++;
+            }
+            for(thread& t: threads) {
+                t.join();
+            }
+        } catch(exception& e) {
+            cerr << e.what() << endl;
+        }
+    }
+    virtual SandboxThread* clone() const override {
+        return new SandboxThread();
+    }
+};
+int test_Sandbox_Thread() {
+    cout << "----------------------- test_Sandbox_Thread" << endl;
+    try {
+        SandboxThread sandbox;
+        sandbox.doThread_A_B_C();
+        SandboxThread* tmp = sandbox.clone();
+        tmp->doThread_A_B_C();
+        delete tmp;
+    } catch( exception& e ) {
+        cerr << e.what() << endl;
+        return -1;
+    }
+    return 0;
+}
+// うん、スレッドはこんなんが、今は理想だと思う。
+// 同じ処理やループ、同期を必要とするのは理想的ではない。
+// 最終的には、prototype と関係なくなってる気もしなくはないね：）
+
 int main() {
     cout << "START Prototype ===============" << endl;
     ptr_lambda_debug<const char*,const int&>("Here we go.",0);
@@ -209,6 +318,9 @@ int main() {
     }
     if(2) {
         ptr_lambda_debug<const string&,const int&>("Play and Result ... ", test_Thread_Prototype());
+    }
+    if(3) {
+        ptr_lambda_debug<const string&,const int&>("Play and Result ... ", test_Sandbox_Thread());
     }
     cout << "=============== Prototype END" << endl;
     return 0;
