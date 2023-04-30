877f3b1d0211daa99ff4119d1da9ab241338bd99
alienm14xr2 <alienm14xr2@gmail.com>
2023-04-30 15:50:13 +0900

    GoF Chain of Responsibility パターン、Base とそのテストを作った。 - インタフェースはポインタ、これ重要だ。

--- src/gof-chain_of_responsibility.cpp
+++ src/gof-chain_of_responsibility.cpp
@@ -11,6 +11,8 @@
   Handler インタフェースは next として、次の責任者、Handler オブジェクトをメンバ変数に持つ。
   Handler インタフェースはメンバ関数として request を持つ。
 
+  オレの解釈、動的Template Method とFactory Method が合わさった感じ。
+  使い方は沢山あるように思える。ではいつものように基本から。
 */
 #include <iostream>
 #include <cassert>
@@ -19,11 +21,109 @@ using namespace std;
 
 template<class M,class D>
 void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
-    cout << message << '\t' << endl;
+    cout << message << '\t' << debug << endl;
 };
 
+class Handler {
+protected:
+    Handler* next;
+public:
+    virtual ~Handler() {}
+    virtual int request() const = 0;
+};
+class ConcreteHandlerA final : public virtual Handler {
+    // この初期化定義ができるのは自身のメンバ変数のみだと知る。
+    // ConcreteHandlerA():foo{nullptr}bar{-1} {}
+public:
+    ConcreteHandlerA() {
+        this->next = nullptr;
+    }
+    ConcreteHandlerA(Handler& handler) {
+        this->next = &handler;
+    }
+    ConcreteHandlerA(const ConcreteHandlerA& own) {
+        *this = own;
+        this->next = own.next;
+    }
+    ~ConcreteHandlerA() {}
+    virtual int request() const override {
+        cout << "called request of ConcreteHandlerA ... " << endl;
+        if( this->next != nullptr ) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+};
+class ConcreteHandlerB final : public virtual Handler {
+public:
+    ConcreteHandlerB() {
+        this->next = nullptr;
+    }
+    ConcreteHandlerB(Handler& handler) {
+        this->next = &handler;
+    }
+    ConcreteHandlerB(const ConcreteHandlerB& own) {
+        *this = own;
+        this->next = own.next;
+    }
+    ~ConcreteHandlerB() {}
+    virtual int request() const override {
+        cout << "called request of ConcreteHandlerB ... " << endl;
+        if( this->next != nullptr ) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+};
+class ConcreteHandlerC final : public virtual Handler {
+public:
+    ConcreteHandlerC(){
+        this->next = nullptr;
+    }
+    ConcreteHandlerC(Handler& handler) {
+        this->next = &handler;
+    }
+    ConcreteHandlerC(const ConcreteHandlerC& own) {
+        *this = own;
+        this->next = own.next;
+    }
+    ~ConcreteHandlerC() {}
+    virtual int request() const override {
+        cout << "called request of ConcreteHandlerC ... " << endl;
+        if( this->next != nullptr ) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+};
+int test_basic_chain_of_responsibility() {
+    cout << "------------------------------------ test_basic_chain_of_responsibility" << endl;
+    ConcreteHandlerC handlerC;
+    ConcreteHandlerB handlerB(handlerC);
+    ConcreteHandlerA handlerA(handlerB);
+    Handler* handler = static_cast<Handler*>(&handlerA);
+    int ret  = handler->request();
+    ptr_lambda_debug<const string&,const int&>("ret is ",ret);
+    handler = static_cast<Handler*>(&handlerB);
+    ret = handler->request();
+    ptr_lambda_debug<const string&,const int&>("ret is ",ret);
+    handler = static_cast<Handler*>(&handlerC);
+    ret = handler->request();
+    ptr_lambda_debug<const string&,const int&>("ret is ",ret);
+    // これも大切な気づき、勉強になった。インタフェース（抽象基底クラス）で操作する場合、そのクラスはポインタにすること。
+    // でないと、意図した動きにならない。色々と試してみないと分からないね。
+    // インタフェースはポインタ、これが新たなルール。
+
+    return 0;
+}
 int main() {
     cout << "START GoF Chain of Responsibility ===============" << endl;
+    if(1) {
+        ptr_lambda_debug<const string&,const int&>("Play and Result ... ",test_basic_chain_of_responsibility());
+    }
     cout << "=============== GoF Chain of Responsibility END" << endl;
     return 0;
 }
