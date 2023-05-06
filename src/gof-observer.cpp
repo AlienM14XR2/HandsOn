e54d38e50f910c15c846a266dfae5a5218105fd5
alienm14xr2 <alienm14xr2@gmail.com>
2023-05-06 18:01:55 +0900

    GoF Observer パターン。 - gof-observer.cpp 作成、実装はまだ。

---
+++ src/gof-observer.cpp
@@ -0,0 +1,15 @@
+/**
+  Observer パターン
+
+  はい、知りません：）なんだっけ？
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "START Observer ===============" << endl;
+    cout << "=============== Observer END" << endl;
+    return 0;
+}
