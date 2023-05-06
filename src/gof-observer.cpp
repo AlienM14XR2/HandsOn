090f6d6fd6bf57abde12491df99ae4bac73e3c10
alienm14xr2 <alienm14xr2@gmail.com>
2023-05-07 07:31:59 +0900

    GoF Observer パターン。 - Observer パターンを文章から理解（不完全）。

--- src/gof-observer.cpp
+++ src/gof-observer.cpp
@@ -3,6 +3,19 @@
 
   はい、知りません：）なんだっけ？
 
+  Observer パターンを学びます。Observer とは、英語で「観察者」を意味する単語です。
+  Observer パターンとは、状態の変化を観察することを目的としたものですが、どちらかというと
+  「観察」よりも「通知」に重点の置かれたものになっています。
+
+  だそうです。
+
+  Subject、 observerList をメンバ変数に持つ。Observer を集約している。
+  addObserver メンバ関数、notifyObserver メンバ関数、done メンバ関数を持つ。
+  Observer、 update メンバ関数を宣言したインタフェース。
+  Subject Observer それぞれに派生クラスが複数存在する。
+
+  そんな、クラス図。
+  やっぱり、分からないから基本のクラスを作っていく。
 */
 #include <iostream>
 
