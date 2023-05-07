ea0bbf90c2b7f72d245d56733f7eedc284b9a87f
alienm14xr2 <alienm14xr2@gmail.com>
2023-05-08 07:36:42 +0900

    GoF Observer パターン。 - 基本実装を少し進めた。

--- src/gof-observer.cpp
+++ src/gof-observer.cpp
@@ -10,7 +10,7 @@
   だそうです。
 
   Subject、 observerList をメンバ変数に持つ。Observer を集約している。
-  addObserver メンバ関数、notifyObserver メンバ関数、done メンバ関数を持つ。
+  addObserver メンバ関数、notifyObservers メンバ関数、done メンバ関数を持つ。
   Observer、 update メンバ関数を宣言したインタフェース。
   Subject Observer それぞれに派生クラスが複数存在する。
 
@@ -18,8 +18,49 @@
   やっぱり、分からないから基本のクラスを作っていく。
 */
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+class Observer {
+public:
+    virtual ~Observer() {}
+    virtual int update() const = 0; 
+};
+class Subject {
+protected:
+    vector<Observer*> observerList;
+public:
+    Subject() {}
+    Subject(const Subject& own) {
+        *this = own;
+        this->observerList = own.observerList;
+    }
+    virtual ~Subject() {}
+    void addObserver(Observer* ob) noexcept {
+        observerList.push_back(ob);
+    }
+    void notifyObservers() noexcept {
+        for(Observer* o: observerList) {
+            o->update();
+        }
+    }
+    virtual int done() const = 0;
+};
+class ConcreteSubjectA final : public virtual Subject {
+public:
+    ConcreteSubjectA() {}
+    ConcreteSubjectA(const ConcreteSubjectA& own) {
+        *this = own;
+        this->observerList = own.observerList;
+    }
+    ~ConcreteSubjectA() {}
+    virtual int done() const override {
+        cout << "done ... ConcreteSubjectA." << endl;
+        return 0;
+    }
+};
 
 int main() {
     cout << "START Observer ===============" << endl;
