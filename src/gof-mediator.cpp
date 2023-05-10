875a10c7447eae19576398af5dc93e2fdebd31bb
alienm14xr2 <alienm14xr2@gmail.com>
2023-05-11 00:22:10 +0900

    GoF Mediator パターン。 - ファイルを作成しただけ：）ごめん、もう眠い。

---
+++ src/gof-mediator.cpp
@@ -0,0 +1,20 @@
+/**
+  GoF Mediator パターン
+
+  仲介者、直訳すると。
+  http://www.javacamp.org/designPattern/mediator.html
+
+  Facade Command Observer の各パターンは簡単になめてきた。
+  なので、多少はMediator にかんしても理解が容易になっているはず。
+  うん、もう眠くなってしまった。テンプレートを作成して寝る。
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Start GoF Mediator ===============" << endl;
+    cout << "=============== GoF Mediator END" << endl;
+    return 0;
+}
