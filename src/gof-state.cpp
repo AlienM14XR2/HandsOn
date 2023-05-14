b1d965071511216bffde363650782b18c560f41c
alienm14xr2 <alienm14xr2@gmail.com>
2023-05-15 06:15:31 +0900

    GoF State パターン。 - ファイルを作成しただけ。 - State の理解とそのクラス解析はまだ。

---
+++ src/gof-state.cpp
@@ -0,0 +1,13 @@
+/**
+  GoF State パターン
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "START GoF State ===============" << endl;
+    cout << "=============== GoF State END" << endl;
+    return 0;
+}
