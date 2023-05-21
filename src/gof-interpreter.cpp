195e69fd4bc81600bee387d1325c914b702afe6b
alienm14xr2 <alienm14xr2@gmail.com>
2023-05-21 23:17:51 +0900

    GoF Interpreter パターン。 - ひとまず、テンプレートファイルを作成しただけ。

---
+++ src/gof-interpreter.cpp
@@ -0,0 +1,15 @@
+/**
+  GoF Interpreter 
+
+
+*/
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    cout << "START GoF Interpreter ===============" << endl;
+    cout << "=============== GoF Interpreter END" << endl;
+    return 0;
+}
