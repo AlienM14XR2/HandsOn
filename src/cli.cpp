89c8fb545f29824b23d089ef4c4cd1ccfd71829c
alienm14xr2 <alienm14xr2@gmail.com>
2023-06-11 20:54:45 +0900

    cli.cpp 標準入出力、その入力、出力表現の学習。。 - C で書いたデバッグ関数とCLI の基本コンセプト関数を移植した。

--- src/cli.cpp
+++ src/cli.cpp
@@ -5,22 +5,146 @@
     今後の開発はC++ も利用するが、ベースはC で行う予定。まずは、いつものテンプレソースファイルを用意し
     その後cli.c で有効だった関数を移植する予定。
 
+    @Author Jack
 */
 #include <iostream>
-
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "ctype.h"
 using namespace std;
 
+#define CMD_SIZE       1024
+#define CMD_SPLIT_SIZE   32
+
+typedef struct {
+    int no;
+    char data[CMD_SPLIT_SIZE];
+} CMD_DATA;
+
+// Debug 用の関数
 template<class M,class D>
 void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
     cout << message << '\t' << debug << endl;
 };
 // C のprintf が優秀なのでほしい、そのデバッグも利用したい：）
+void println(const char* message){
+    printf("%s\n",message);
+}
+void ptr_d_debug(const char* message, int* debug) {
+    printf("%s\tvalue=%d\taddr=%p\n",message,*debug,debug);
+}
+void ptr_lf_debug(const char* message, double* debug) {
+    printf("%s\tvalue=%lf\taddr=%p\n",message,*debug,debug);
+}
+void ptr_str_debug(const char* message, char* debug) {
+    printf("%s\tvalue=%s\taddr=%p\n",message,debug,debug);
+}
+void ptr_cstr_debug(const char* message, const char* debug) {
+    printf("%s\tvalue=%s\taddr=%p\n",message,debug,debug);
+}
 
+int monitoring();
 int main(void) {
     cout << "START CLI ===============" << endl;
     if(1.0) {
         ptr_lambda_debug<const string&,const int&>("Yeah here we go.",0);
+        println("It's println. C/C++.");
+        int x = 3;
+        ptr_d_debug("x is ",&x);
+        double y = 3.141592;
+        ptr_lf_debug("y is ",&y);
+        char str[] = {"It's my life.\0"};
+        ptr_str_debug("char str[] is",str);
+        const char cstr[] = {"It's my PRIDE."};
+        ptr_cstr_debug("const char cstr is ",cstr);
+    }
+    if(10) {
+        monitoring();
     }
     cout << "=============== CLI END" << endl;
+    return 0;
+}
+
+int handler(char*);
+int cmd_exit(char*);
+int cmd_create(char*);
+int upper_str(const char*, char*);
+int init_cmd(char*);
+int init_io(char*,char*);
+int monitoring() {
+    char cmd[CMD_SIZE]={'\0'};
+    char cmd_upper[CMD_SIZE]={'\0'};
+    printf("ORx2> ");    
+    scanf("%s", cmd);
+    while(1) {
+        upper_str(cmd,cmd_upper);
+//        ptr_str_debug("cmd_upper is",cmd_upper);
+        // handler には必ず、Upper された文字列を実引数にすること。
+        if( handler(cmd_upper) == 1 ) {   // 1 が返却された時はループを抜ける。
+            break;
+        }
+        init_io(cmd,cmd_upper);
+    }
+    return 0;
+}
+int init_cmd(char* cmd) {
+    for(int i = 0; i < sizeof(cmd)/sizeof(cmd[0]); i++) {
+        cmd[i] = '\0';
+    }
+    return 0;
+}
+/**
+    これでは、問題があることに気付いた。
+  Values はユーザの任意なので、これを大文字に強制的に変換してしまうのは違う。
+  したがって、別途、Values 以降の（）内の値は無視するものが必要だ。
+    
+*/
+int upper_str(const char* in, char* out) {
+    int i = 0;
+    while(in[i] != '\0') {
+        out[i] = toupper(in[i]);
+        i++;
+    }
+    return 0;
+}
+int init_io(char* cmd,char* cmd_upper) {
+    printf("ORx2> ");    
+    init_cmd(cmd);
+    init_cmd(cmd_upper);
+    scanf("%s", cmd);
+    return 0;
+}
+int handler(char* cmd) {
+    if(cmd_exit(cmd) == 1) {
+        return 1;
+    }
+    cmd_create(cmd);
+    return 0;
+}
+int cmd_exit(char* cmd) {
+    if(strcmp("EXIT",cmd) == 0) {
+        println("Bye :)");
+        return 1;
+    }
+    return 0;
+}
+int cmd_create(char* cmd) {
+    // ここからはTrim して半角スペースの除去が必要かもしれない。
+    if(strcmp("CREATE",cmd) == 0) {
+        println("It's create.");
+    }
+    return 0;
+}
+/**
+    is End of Command.
+        入力されたコマンドの終端を検知する。
+    0 is no hit.
+    1 is hit.
+*/
+int is_eoc(const char* c) {
+    if(*c == ';' || *c == '\0') {
+        return 1;
+    }
     return 0;
 }
