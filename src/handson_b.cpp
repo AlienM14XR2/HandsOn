13cc0abef027ebb847eef3b79cac142a54ac490d
alienm14xr2 <alienm14xr2@gmail.com>
2023-07-28 11:39:53 +0900

    handson_b また適当な名前にしてしまった、センスがない。 - 継承における自己課題、確認が目的。 - 納得したのでこれでこのソース、課題は終了。

--- src/handson_b.cpp
+++ src/handson_b.cpp
@@ -15,12 +15,13 @@ void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
 class IFace {
 protected:
     virtual int pro_a() const = 0;
+    virtual int pro_b() const = 0;
 public:
     virtual int pub_a() const = 0;
+    virtual int pub_b() const = 0;
     virtual ~IFace() {};
 };
-
-class Foo final : public IFace {
+class Foo : public IFace {
 private:
     int pri_a() {
         ptr_lambda_debug<const string&,const int&>("Foo ... pri_a.",0);
@@ -34,6 +35,10 @@ protected:
     virtual int pro_a() const override {
         ptr_lambda_debug<const string&,const int&>("Foo ... pro_a.",0);
 //        pri_b();        // やっぱりこれもコンパイルエラー、理由は同じだ。
+        return 0;
+    }
+    virtual int pro_b() const override {
+        ptr_lambda_debug<const string&,const int&>("Foo ... pro_b.",0);
         return 0;
     }
 public:
@@ -50,6 +55,25 @@ public:
         pro_a();
         return 0;
     }
+    virtual int pub_b() const override {
+        ptr_lambda_debug<const string&,const int&>("Foo ... pub_b.",0);
+        pro_b();
+        return 0;
+    }
+};
+class Bar final : public Foo {
+public:
+    Bar() {}
+    Bar(const Bar& own) {
+        (*this) = own;
+    }
+    ~Bar() {}
+    virtual int pub_b() const override {
+        ptr_lambda_debug<const string&,const int&>("Bar ... pub_b.",0);
+        pro_b();
+        return 0;
+    }
+
 };
 
 int main(void) {
@@ -59,9 +83,20 @@ int main(void) {
     }
     if(2) {
         Foo foo;
-        ptr_lambda_debug<const string&,const int&>("Play and Result ... foo",foo.pub_a());
+        ptr_lambda_debug<const string&,const int&>("Play and Result ... foo.pub_a()",foo.pub_a());
         IFace* iface = static_cast<IFace*>(&foo);
-        ptr_lambda_debug<const string&,const int&>("Play and Result ... iface",iface->pub_a());
+        ptr_lambda_debug<const string&,const int&>("Play and Result ... iface->pub_a()",iface->pub_a());
+        ptr_lambda_debug<const string&,const int&>("Play and Result ... iface->pub_b()",iface->pub_b());
+        Bar bar;
+        ptr_lambda_debug<const string&,const int&>("Play and Result ... bar.pub_a()",bar.pub_a());  // 予想通りだよね：）
+        iface = static_cast<IFace*>(&bar);
+        ptr_lambda_debug<const string&,const int&>("Play and Result ... iface->pub_a()",iface->pub_a());
+        ptr_lambda_debug<const string&,const int&>("Play and Result ... iface->pub_b()",iface->pub_b());
+        // うん、気持ちがいいね、よくわかった。
+        // 基底クラスは置いた方が何かといいのかもしれないな：）
+        // インタフェース、基底クラス、派生クラス、これが C++ における OOP の基本的なかたちだな、やはり。
+        // 蛇足だけど Java でも同じだと思う。
+
     }
     cout << "=============== handson_b END" << endl;
     return 0;
