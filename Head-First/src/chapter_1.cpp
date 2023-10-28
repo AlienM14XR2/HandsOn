2e095c22e9fd8609dcb8f00a4c4097afe92ad4ed
Jack <alienm14xr2@gmail.com>
2023-10-28 18:14:09 +0900

    Head First デザインパターン - １章    デザインパターン - 飛び方、鳴き方のインタフェースとその具象化クラスを実装した。

--- Head-First/src/chapter_1.cpp
+++ Head-First/src/chapter_1.cpp
@@ -3,6 +3,7 @@
  * １章 デザインパターン入門
  * 
  * ```
+ * e.g. コンパイル
  * g++ -O3 -std=c++20 -pedantic-errors -Wall -Werror chapter_1.cpp -o ../bin/main
  * ```
 */
@@ -14,6 +15,54 @@ template< class M, class D>
 void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
     cout << "DEBUG: " << message << '\t' << debug << endl;
 };
+
+/**
+ * 飛び方のインタフェース
+*/
+class FlyBehavior {
+public:
+    virtual ~FlyBehavior() {}
+    virtual void fly() const = 0;
+};
+
+/**
+ * 鳴き方のインタフェース
+*/
+class QuackBehavior {
+public:
+    virtual ~QuackBehavior() {}
+    virtual void quack() const = 0;
+};
+
+/**
+ * 飛び方の具象化クラス
+ * 
+ * 翼で飛べる。
+*/
+class FlyWithWing final : public virtual FlyBehavior {
+public:
+    FlyWithWing() noexcept {}
+    FlyWithWing(const FlyWithWing& own) noexcept {
+        (*this) = own;
+    }
+    ~FlyWithWing() noexcept {}
+    virtual void fly() const override {
+        puts("飛んでいます！！");
+    }
+};
+
+class FlyNoWay final : public virtual FlyBehavior {
+public:
+    FlyNoWay() noexcept {}
+    FlyNoWay(const FlyNoWay& own) noexcept {
+        (*this) = own;
+    }
+    ~FlyNoWay() noexcept {}
+    virtual void fly() const override {
+        puts("飛べません");
+    }
+};
+
 
 /**
  * Duck（カモ） 抽象クラス
@@ -36,9 +85,29 @@ public:
  * マガモ クラス
  * カモの具象化クラス
 */
-class MallardDuck : public virtual Duck {
-
+class MallardDuck final : public virtual Duck {
+public:
+    MallardDuck() {}
+    MallardDuck(const MallardDuck& own) {
+        (*this) = own;
+    }
+    ~MallardDuck() {}
+    virtual void display() const override {
+        puts("私は本物のマガモです");
+    }
 };
+
+int testMallardDuck() {
+    puts("--- testMallardDuck");
+    try {
+        MallardDuck mallardDuck;
+        mallardDuck.display();
+    } catch(exception& e) {
+        cout << e.what() << endl;
+        return 1;
+    }
+    return 0;
+}
 
 int main(void) {
     puts(" ========= START １章 デザインパターン入門");
@@ -47,7 +116,9 @@ int main(void) {
         double pi = 3.141592;
         ptr_lambda_debug<const char*, const int&>("n is ",n);
         ptr_lambda_debug<const char*, const double&>("pi is ",pi);
-
+    }
+    if(1.01) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result .... ",testMallardDuck());
     }
     puts("１章 デザインパターン入門 END =========");
 }
