254b2498b983e6da78efbc744583d4d859030389
Jack <alienm14xr2@gmail.com>
2023-11-01 22:46:34 +0900

    Head First デザインパターン - ２章 Observer パターン 気象観測所の実装開始 - Observer のリファクタリングを行った。 - 未テスト。

--- Head-First/src/chapter_2_observer.cpp
+++ Head-First/src/chapter_2_observer.cpp
@@ -52,7 +52,6 @@ void (*ptr_labmda_debug)(M,D) = [](auto message, auto debug) -> void {
 
 class Observer {
 protected:
-    size_t index;
 public:
     Observer() {}
     Observer(const Observer& own) {
@@ -60,12 +59,6 @@ public:
     }
     virtual ~Observer() {}
     virtual void update(const double temp, const double humidity, const double pressure) const = 0;     // 気温、湿度、気圧 ... 気象測定値に変化があるとObserver が Subject から取得される測定値です。
-    size_t getIndex() {
-        return index;
-    }
-    void setIndex(const size_t i) {
-        index = i;
-    }
 };
 
 class Subject {
@@ -105,7 +98,6 @@ public:
     }
     virtual void registerObserver(Observer* o) const override {
         observers->push_back(o);
-        o->setIndex(observers->size() - 1);
     }
     virtual void removeObserver(Observer* o) const override {
         // TODO vector erase の調査
