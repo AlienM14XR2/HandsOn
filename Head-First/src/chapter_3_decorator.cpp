820c173a86b79f66d8b15778ed93a8f701a0cb70
Jack <alienm14xr2@gmail.com>
2023-11-04 20:16:09 +0900

    Head First デザインパターン - ３章 Decorator パターン - オブジェクトの装飾。 - 空の chapter_3_decorator.cpp を用意した。

---
+++ Head-First/src/chapter_3_decorator.cpp
@@ -0,0 +1,16 @@
+/**
+ * 3 章 Decorator
+ * 
+ * g++ -O3 -std=c++20 -pedantic-errors -Wall -Werror chapter_3_decorator.cpp -o ../bin/main
+ * 
+ * オブジェクトの装飾
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    puts("START 3 章 Decorator =========");
+    puts("========= END 3 章 Decorator ");
+    return 0;
+}
