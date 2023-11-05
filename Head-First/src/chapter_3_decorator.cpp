a4cb86963aae95ae6a28e3b90dd29e020364881e
Jack <alienm14xr2@gmail.com>
2023-11-05 18:39:58 +0900

    Head First デザインパターン - ３章 Decorator パターン - コーヒーショップのレジシステム、サンプル実装中。 - 未テスト。

--- Head-First/src/chapter_3_decorator.cpp
+++ Head-First/src/chapter_3_decorator.cpp
@@ -9,6 +9,8 @@
  * 
  * Decorator パターンはオブジェクトに追加の責務を動的に付与する。デコレータは、サブクラス化の
  * 代替となる、柔軟な機能拡張手段を備えている。
+ * 
+ * コーヒーショップのレジシステムを想定したサンプルになる。
 */
 #include <iostream>
 
@@ -17,6 +19,85 @@ using namespace std;
 template <class M, class D>
 void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
     cout << "DEBUG: " << message << '\t' << debug << endl;
+};
+
+/**
+ * 飲み物の基底クラス
+ * 飲み物の抽象クラス
+*/
+class Beverage {
+protected:
+    string description = "不明な飲み物";
+public:
+    virtual ~Beverage() {}
+    string getDescription() noexcept {
+        return description;
+    }
+    virtual double cost() const = 0;
+};
+
+/**
+ * 調味料の基底クラス（Decorator）
+ * 調味料の抽象クラス
+ * Beverage のサブクラス
+*/
+class CondimentDecorator : public virtual Beverage {
+protected:
+    Beverage* beverage;
+public:
+    ~CondimentDecorator() {}
+    virtual string getDescription() const = 0; 
+};
+
+/**
+ * 飲み物、エスプレッソ クラス
+*/
+class Espresso final : public virtual Beverage {
+public:
+    Espresso() {
+        description = "エスプレッソ";
+    }
+    Espresso(const Espresso& own) { *this = own; }
+    ~Espresso() {}
+    virtual double cost() const override {
+        return 1.99;
+    }
+};
+
+/**
+ * 飲み物、ブレンド クラス
+*/
+class HouseBlend final : public virtual Beverage {
+public:
+    HouseBlend() {
+        description = "ハウスブレンドコーヒー";
+    }
+    HouseBlend(const HouseBlend& own) {*this = own;}
+    ~HouseBlend() {}
+    virtual double cost() const override {
+        return 0.89;
+    }
+};
+
+/**
+ * モカ
+ * 調味料の具象クラス
+*/
+class Mocha final : public virtual CondimentDecorator {
+public:
+    Mocha(Beverage& b) {
+        beverage = &b;
+    }
+    Mocha(const Mocha& own) {*this = own;}
+    ~Mocha() {}
+
+    virtual string getDescription() const override {
+        string result = beverage->getDescription() + "、モカ";
+        return result;
+    }
+    virtual double cost() const override {
+        return beverage->cost() + 0.20;
+    }
 };
 
 int main(void) {
