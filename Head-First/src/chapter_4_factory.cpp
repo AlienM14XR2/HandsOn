f3ba48511e2755e17c851641237cee6179e0854b
Jack <alienm14xr2@gmail.com>
2023-11-08 11:51:29 +0900

    Head First デザインパターン - ４章 Factory パターン - Pizza インタフェースとPizzaStore クラスの実装を行った。 - 未テスト だよ：）。

--- Head-First/src/chapter_4_factory.cpp
+++ Head-First/src/chapter_4_factory.cpp
@@ -5,6 +5,8 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_4_factory.cpp -o ../bin/main
  * 
  * OO の利点を活用した構築
+ * 
+ * サンプルは様々な地域に出店するピザ屋のシステム。
 */
 #include <iostream>
 
@@ -13,6 +15,33 @@ using namespace std;
 template<class M, class D>
 void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
     cout << "DEBUG: " << message << '\t' << debug << endl;
+};
+
+class Pizza {
+public:
+    virtual ~Pizza() {}
+    virtual void prepare() const = 0;
+    virtual void bake() const = 0;
+    virtual void cut() const = 0;
+    virtual void box() const = 0;
+};
+
+class PizzaStore {
+protected:
+    Pizza* pizza = nullptr;
+    virtual Pizza& createPizza(const string& type) const = 0;
+public:
+    PizzaStore() {}
+    PizzaStore(const PizzaStore& own) {*this = own;}
+    virtual ~PizzaStore() {}
+    Pizza& orderPizza(const string& type) {
+        *pizza = createPizza(type);
+        pizza->prepare();
+        pizza->bake();
+        pizza->cut();
+        pizza->box();
+        return *pizza;
+    }    
 };
 
 int main() {
