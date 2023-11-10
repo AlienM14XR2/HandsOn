34fade5a14fda5946832c336ce425c725c4e84b4
Jack <alienm14xr2@gmail.com>
2023-11-10 23:11:09 +0900

    Head First デザインパターン - ６章 Command パターン - Command インタフェースを実装した。 - レシーバとしての照明クラスを実装し、そのコマンドの具象クラスも実装した（未テスト）。

--- Head-First/src/chapter_6_command.cpp
+++ Head-First/src/chapter_6_command.cpp
@@ -5,6 +5,8 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_6_command.cpp -o ../bin/main
  * 
  * 呼び出しのカプセル化
+ * 
+ * リモコン API をサンプルにする。
 */
 #include <iostream>
 
@@ -13,6 +15,50 @@ using namespace std;
 template<class M, class D>
 void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
     cout << "DEBUG: " << message << '\t' << debug << endl;
+};
+
+/**
+ * Command インタフェース
+*/
+class Command {
+public:
+    virtual ~Command() {}
+    virtual void execute() const = 0;
+};
+
+/**
+ * 照明クラス
+*/
+class Light {
+public:
+    Light() {}
+    Light(const Light& own) {*this = own;}
+    ~Light() {}
+    void on() {
+        ptr_lambda_debug<const char*,const int&>("Light on",0);
+    }
+    void off() {
+        ptr_lambda_debug<const char*,const int&>("Light off",0);
+    }
+};
+
+/**
+ * 照明をつけるコマンドを実装する。
+*/
+class LightCommand final : public virtual Command {
+private:
+    mutable Light light;
+    LightCommand() {}
+public:
+    LightCommand(const Light& l) {
+        light = l;
+    }
+    LightCommand(const LightCommand& own) {*this = own;}
+    ~LightCommand() {}
+    
+    virtual void execute() const override {
+        light.on();
+    }
 };
 
 int main(void) {
