101b2f17ede358004692e999db3acd2e6a42bbd0
Jack <alienm14xr2@gmail.com>
2023-11-17 02:46:23 +0900

    Head First デザインパターン - ７章 Adapter パターンと Facade パターン - テンプレソースファイルを用意した。 - chapter_7_adapter_facade.cpp

---
+++ Head-First/src/chapter_7_adapter_facade.cpp
@@ -0,0 +1,17 @@
+/**
+ * 7 章 Adapter パターンと Facade パターン
+ * 
+ * 適合可能にする。
+ * 
+ * e.g. compile
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_7_adapter_facade.cpp -o ../bin/main
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    puts("START 7 章 Adapter パターンと Facade パターン ===");
+    puts("=== 7 章 Adapter パターンと Facade パターン END");
+    return 0;
+}
