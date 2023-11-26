0938633ec26b72ccd590273fe351548d64f5b54c
Jack <alienm14xr2@gmail.com>
2023-11-26 19:02:11 +0900

    Head First デザインパターン - ８章 Template Method パターン - Coffee Tea クラスを実装した、Template Methodパターン（CaffeineBeverage.prepareRecipe()）の利用。 - 各サブクラスのテストを実装した。

--- Head-First/src/chapter_8_template_method.cpp
+++ Head-First/src/chapter_8_template_method.cpp
@@ -9,6 +9,7 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_8_template_method.cpp -o ../bin/main
 */
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -27,13 +28,21 @@ void (*ptr_lambda_debug)(M, D) = [](auto message, auto debug) -> void {
 class CaffeineBeverage {
 public:
     virtual ~CaffeineBeverage() {}
-    virtual void prepareRecipe() const = 0;
+    
+    const void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
     void boilWater() {
         puts("お湯を沸かす");
     }
     void pourInCup() {
         puts("カップに注ぐ");
     }
+    virtual void brew() const = 0;
+    virtual void addCondiments() const = 0;
 };
 
 class Coffee final : public virtual CaffeineBeverage {
@@ -41,8 +50,11 @@ public:
     Coffee() {}
     Coffee(const Coffee& own) {*this = own;}
     ~Coffee() {}
-    virtual void prepareRecipe() const override {
-
+    virtual void brew() const override {
+        puts("フィルタを使ってコーヒーをドリップする");
+    }
+    virtual void addCondiments() const override {
+        puts("砂糖とミルクを追加する");
     }
 };
 
@@ -51,16 +63,51 @@ public:
     Tea() {}
     Tea(const Tea& own) {*this = own;}
     ~Tea() {}
-    virtual void prepareRecipe() const override {
-        
+    virtual void brew() const override {
+        puts("紅茶を浸す");
+    }
+    virtual void addCondiments() const override {
+        puts("レモンを追加する");
     }
 };
+
+int test_Coffee() {
+    puts("--- test_Coffee");
+    Coffee coffee;
+    coffee.prepareRecipe();
+    try {
+        return 0;
+    } catch(exception& e) {
+        cout << e.what() << endl;
+        return -1;
+    }
+}
+
+int test_Tea() {
+    puts("--- test_Tea");
+    try {
+        Tea tea;
+        tea.prepareRecipe();
+        return 0;
+    } catch(exception& e) {
+        cout << e.what() << endl;
+        return -1;
+    }
+}
 
 int main(void) {
     puts("START 8 章 Template Method パターン ===");
     if(0.01) {
         double pi = 3.14159;
         ptr_lambda_debug<const char*,const double&>("pi si ",pi);
+    }
+    if(1.01) {
+        int check = -1;
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ",check = test_Coffee());
+        assert(check == 0);
+        check = -1;
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ",check = test_Tea());
+        assert(check == 0);
     }
     puts("=== 8 章 Template Method パターン END");
     return 0;
