e0a58b1a9aa45a53890df708d134b2e80064e0b4
Jack <alienm14xr2@gmail.com>
2023-11-28 15:36:25 +0900

    Head First デザインパターン - ８章 Template Method パターン - Hook を利用してみた。 - そのテストクラスを実装した。

--- Head-First/src/chapter_8_template_method.cpp
+++ Head-First/src/chapter_8_template_method.cpp
@@ -9,11 +9,16 @@
  * 
  * コーヒーショップのコーヒーの作り方、紅茶の作り方をサンプルにする。
  * 
+ * テンプレートメソッドをフックする
+ * フックは抽象クラスで宣言される、空の実装やデフォルトの実装だけを備えたメソッドです。
+ * フックは必要に応じてさまざまな時点でアルゴリズムを「フック（hook）」する機能をサブクラスに与えます。
+ * 
  * e.g. compile
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_8_template_method.cpp -o ../bin/main
 */
 #include <iostream>
 #include <cassert>
+#include <random>
 
 using namespace std;
 
@@ -37,7 +42,12 @@ public:
         boilWater();
         brew();
         pourInCup();
-        addCondiments();
+        if( customerWantsCondiments() ) {
+            addCondiments();
+        }
+    }
+    virtual bool customerWantsCondiments() {
+        return true;
     }
     void boilWater() {
         puts("お湯を沸かす");
@@ -75,6 +85,45 @@ public:
     }
 };
 
+int test_random(unsigned int pattern) {
+    puts("--- test_random");
+    // メルセンヌ・ツイスター法による擬似乱数生成器を、
+    // ハードウェア乱数をシードにして初期化
+    std::random_device seed_gen;
+    std::mt19937 engine(seed_gen());
+    // 一様実数分布
+    // [-1.0, 1.0)の値の範囲で、等確率に実数を生成する
+    std::uniform_real_distribution<> dist1(-1.0, 1.0);
+    double r1 = dist1(engine);
+    cout << r1 << endl;
+
+    int n = (r1*10);
+//    cout << n << endl;
+    int a = n%pattern;
+    if(a < 0) {
+        a *= -1;
+    }
+    cout << a << endl;
+    return a;    
+}
+
+
+class CoffeeWithHook final : public virtual CaffeineBeverage {
+public:
+    CoffeeWithHook() {}
+    CoffeeWithHook(const CoffeeWithHook& own) {*this = own;}
+    ~CoffeeWithHook() {}
+    virtual void brew() const override {
+        puts("フィルタを使ってコーヒーをドリップする");
+    }
+    virtual void addCondiments() const override {
+        puts("\t砂糖とミルクを追加する");
+    }
+    virtual bool customerWantsCondiments() {
+        return test_random(2);
+    }
+};
+
 int test_Coffee() {
     puts("--- test_Coffee");
     Coffee coffee;
@@ -99,6 +148,20 @@ int test_Tea() {
     }
 }
 
+int test_CoffeeWithHook(size_t count) {
+    puts("--- test_CoffeeWithHook");
+    try {
+        CoffeeWithHook coffee;
+        for(size_t i = 0; i < count; i++) {
+            coffee.prepareRecipe();
+        }
+        return 0;
+    } catch(exception& e) {
+        cout << e.what() << endl;
+        return -1;
+    }
+}
+
 int main(void) {
     puts("START 8 章 Template Method パターン ===");
     if(0.01) {
@@ -112,6 +175,9 @@ int main(void) {
         check = -1;
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ",check = test_Tea());
         assert(check == 0);
+    }
+    if(1.02) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ",test_CoffeeWithHook(5));
     }
     puts("=== 8 章 Template Method パターン END");
     return 0;
