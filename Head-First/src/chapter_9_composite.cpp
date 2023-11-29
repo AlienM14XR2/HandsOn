c0df0eb81a071a03125342197d70600361f50333
Jack <alienm14xr2@gmail.com>
2023-11-29 21:46:40 +0900

    Head First デザインパターン - ９章 の続き Composite パターン - Read を進めた。 - いつものテンプレファイルを追加した。

---
+++ Head-First/src/chapter_9_composite.cpp
@@ -0,0 +1,22 @@
+/**
+ * 9 章の続き Composite パターン
+ * 
+ * Composite パターンの定義
+ * 
+ * オブジェクトをツリー構造に構成して部分 ー 全体階層を表現できる。
+ * Composite パターンを使うと、クライアントは個々のオブジェクトと
+ * オブジェクトのコンポジションを統一的に扱うことができる。
+ * 
+ * e.g. compile.
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_9_composite.cpp -o ../bin/main
+ * 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    puts("START 9 章の続き Composite パターン ===");
+    puts("=== 9 章の続き Composite パターン END");
+    return 0;
+}
