cde90e010ebd8c4b4f61b6df1178d1a7e0b1887e
Jack <alienm14xr2@gmail.com>
2023-11-29 09:11:04 +0900

    Head First デザインパターン - ９章 Iterator パターン - Iterator インタフェースを定義。 - サブクラス及びテストは未実装。

--- Head-First/src/chapter_9_iterator.cpp
+++ Head-First/src/chapter_9_iterator.cpp
@@ -3,6 +3,9 @@
  * 
  * 適切に管理されたコレクション
  * 
+ * Iterator パターンとの出会い
+ * 反復処理のカプセル化計画は、実際にうまくいきそうです。
+ * 
  * e.g. compile.
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_9_iterator.cpp -o ../bin/main
 */
@@ -13,6 +16,14 @@ using namespace std;
 template <class M, class D>
 void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
     cout << "DEBUG: " << message << '\t' << debug << endl;
+};
+
+template <class T>
+class Iterator {
+public:
+    virtual ~Iterator() {}
+    virtual bool hasNext() const = 0;
+    virtual T next() const = 0;
 };
 
 int main(void) {
