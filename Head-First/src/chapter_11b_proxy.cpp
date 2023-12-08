d88bc215324263b4074096f205f372274871358d
Jack <alienm14xr2@gmail.com>
2023-12-08 12:29:17 +0900

    Head First デザインパターン - １１章  Proxy パターン （保護プロキシ） - Person インタフェースを実装した。 - 未テスト、サブクラス未実装。

--- Head-First/src/chapter_11b_proxy.cpp
+++ Head-First/src/chapter_11b_proxy.cpp
@@ -4,6 +4,8 @@
  * RMI は私には C++ で書けない（@see chapter_11_proxy.cpp）。
  * ここでは、サンプルの「保護プロキシ」について書いてみる。
  * 
+ * GEEK matchmaking ... これが課題のサンプル。
+ * 
  * e.g. compile
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_11b_proxy.cpp -o ../bin/main
 */
@@ -14,6 +16,22 @@ using namespace std;
 template <class M, class D>
 void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug)-> void {
     cout << "DEBUG: " << message << '\t' << debug << endl;
+};
+
+/**
+ * Person インタフェース
+*/
+class Person {
+public:
+    virtual string getName() const = 0;
+    virtual string getGender() const = 0;
+    virtual string getInterests() const = 0;
+    virtual int getGeekRating() const = 0;
+    
+    virtual void setName(const string& n) const = 0;
+    virtual void setGender(const string& g) const = 0;
+    virtual void setInterests(const string& i) const = 0;
+    virtual void setGeekRating(const int& r) const = 0;
 };
 
 int main(void) {
