7523a15ac41747d61e42817c83262a342ae7ecb9
Jack <alienm14xr2@gmail.com>
2023-12-27 17:41:24 +0900

    C++ ソフトウェア設計 - 4 章 Visitor パターン。 - Draw visitor を実装した。 - テストを実装した。

--- C++-Software-Design/src/chapter_4_acyclic_visitor.cpp
+++ C++-Software-Design/src/chapter_4_acyclic_visitor.cpp
@@ -81,6 +81,8 @@ public:
     }
     void accept(const AbstractVisitor& av) override {
         // ダウンキャストを使うのか... だからタグなのか、AbstractVisitor が。 これなら循環しなくて済むということ。
+        // 厳密にはこれはクロスキャストと呼ばれる、ダウンキャストより更に実行速度が悪い。
+        // 書籍は、現実的な観点からはこの性能では使えないと言っている。
         if( const auto* cv = dynamic_cast<const Visitor<Circle>*>(&av)) {
             cv->visit(*this);
         }
@@ -132,11 +134,39 @@ public:
     }
 }; 
 
+int test_Draw_Circle() {
+    puts("--- test_Draw_Circle");
+    try {
+        Circle circle(3.00,Point{0.0, 10.0});
+        Draw draw;
+        circle.accept(draw);
+        return EXIT_SUCCESS;
+    } catch(...) {
+        return EXIT_FAILURE;
+    }
+}
+
+int test_Draw_Square() {
+    puts("--- test_Draw_Square");
+    try {
+        Square square(6.0,Point{30.0, 60.0});
+        Draw draw;
+        square.accept(draw);
+        return EXIT_SUCCESS;
+    } catch(...) {
+        return EXIT_FAILURE;
+    }
+}
+
 int main(void) {
     puts("START 4 章 Visitor パターン の続き Acyclic Visitor ===");
     if(0.01) {
         double pi = 3.14159;
         ptr_lambda_debug<const char*,const double&>("pi is ",pi);
+    }
+    if(1.00) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ",test_Draw_Circle());
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ",test_Draw_Square());
     }
     puts("=== 4 章 Visitor パターン の続き Acyclic Visitor END");
     return EXIT_SUCCESS;
