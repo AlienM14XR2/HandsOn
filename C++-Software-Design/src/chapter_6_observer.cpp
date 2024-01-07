edf0d3b763a0b6b66a8fcd6974fd6906469890fa
Jack <alienm14xr2@gmail.com>
2024-01-07 16:20:23 +0900

    C++ ソフトウェア設計 - 6 章 Adapter パターン、Observer パターン、CRTP パターン。 - Subject のサンプルとして Person を実装した。 - 未テスト。

--- C++-Software-Design/src/chapter_6_observer.cpp
+++ C++-Software-Design/src/chapter_6_observer.cpp
@@ -20,6 +20,8 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_6_observer.cpp -o ../bin/main
 */
 #include <iostream>
+#include <string>
+#include <set>
 
 using namespace std;
 
@@ -30,12 +32,15 @@ void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
 
 /**
  * Observer パターンの古典的実装
+ * 
+ * 複数存在する状態変化へ対応するためにテンプレートを利用するように改変した。
 */
 
+template<class Subject, class StateTag>
 class Observer {
 public:
     virtual ~Observer() = default;
-    virtual void update() = 0;
+    virtual void update(const Subject& subject, StateTag property) = 0;
     /**
      * 次のようにも定義できる。
      * 
@@ -43,6 +48,59 @@ public:
      * virtual void update2( arguments representing the updated state ) = 0
     */
 
+};
+
+/**
+ * 観察対象となる人を表す Person クラス
+*/
+
+class Person {
+public:
+    enum StateChange {
+        forenameChanged,
+        surnameChanged,
+        addressChanged,
+    };
+
+    using PersonObserver = Observer<Person,StateChange>;
+
+    explicit Person(string fname, string sname): forename{move(fname)},surname{move(sname)} {}
+    Person(const Person& own) {*this = own;}
+    ~Person() {}
+
+    bool attach(PersonObserver* po) {
+        auto [pos,success] = observers.insert(po);  // この書き方初めて見る、pair を返却するからか。
+        return success;
+    }
+    bool detach(PersonObserver* po) {
+        return (observers.erase(po) > 0U);          // 最後の 0U 興味深い。
+    }
+    void notify(StateChange property) {
+        for(auto iter = begin(observers); iter != end(observers); ) {
+            const auto pos = iter++;
+            (*pos)->update(*this,property);         // Observer（観察者）に通知している。
+        }
+    }
+    void setForename(string newForename) {
+        forename = move(newForename);
+        notify(forenameChanged);
+    }
+    void setSurname(string newSurname) {
+        surname = move(newSurname);
+        notify(surnameChanged);
+    }
+    void setAddress(string newAddress) {
+        address = move(newAddress);
+        notify(addressChanged);
+    }
+    const string& getForename() const { return forename; }
+    const string& getSurname() const { return surname; }
+    const string& getAddress() const { return address; }
+private:
+    string forename;
+    string surname;
+    string address;
+    set<PersonObserver*> observers;
 };
 
 int main(void) {
