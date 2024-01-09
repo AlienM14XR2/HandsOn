902f3ae6e78a01b421b7755154495441c485e3f1
Jack <alienm14xr2@gmail.com>
2024-01-10 00:56:40 +0900

    C++ ソフトウェア設計 - 7 章 Bridge パターン、Prototype パターン、External Polymorphism パターン。 - Bridge パターンの完全版のためのリファクタを行った。 - テストを実装した。

--- C++-Software-Design/src/chapter_7_bridge.cpp
+++ C++-Software-Design/src/chapter_7_bridge.cpp
@@ -3,6 +3,14 @@
  * 
  * ガイドライン 28 ：物理的依存関係を排除するには Bridge パターン
  * 
+ * 解明 Bridge パターン
+ * Bridge パターンは 1994 年に発行された GoF 本にある古典的デザインパターンの 1 つで、
+ * その目的は抽象化により実装詳細をカプセル化し、物理的依存関係を最小化することにあり
+ * ます。C++ では、将来の変更を容易にする、コンパイル時ファイアウォールとして機能します。
+ * 
+ * Bridge パターン
+ * 目的：抽出されたクラスと実装を分離して、それらを独立に変更できるようにする。
+ * 
  * e.g. compile
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_7_bridge.cpp -o ../bin/main
 */
@@ -51,20 +59,38 @@ private:
 };
 
 /**
+ * 自動車を表現する基底クラス
+*/
+class Car {
+private:
+    std::unique_ptr<Engine> pimpl_;     // Pointer-to-implementation
+    // ... more car-specific data members (wheel, drivetrain ... etc)
+protected:
+    explicit Car(std::unique_ptr<Engine> engine):pimpl_(std::move(engine)) {}
+    Engine*       getEngine()       { return pimpl_.get(); }
+    const Engine* getEngine() const { return pimpl_.get(); }
+public:
+    virtual ~Car() = default;
+    virtual void drive() = 0;
+    virtual void getoff() = 0;
+};
+
+/**
  * 電気自動車クラス
 */
-class ElectricCar {
+class ElectricCar : public Car {
 private:
-    std::unique_ptr<Engine> engine_;
+    // std::unique_ptr<Engine> engine_;
 public:
-    explicit ElectricCar(/* mybe some engine arguments */):engine_{std::make_unique<ElectricEngine>(/* engine arguments */)} {}
-    void drive() {
+//    explicit ElectricCar():engine_{std::make_unique<ElectricEngine>()} {}
+    explicit ElectricCar(/* maybe some engine arguments */): Car(std::make_unique<ElectricEngine>(/* engine arguments */)) {}
+    void drive() override {
         puts("--- ElectricCar.drive()");
-        engine_->start();
+        getEngine()->start();
     }
-    void getoff() {
+    void getoff() override {
         puts("--- ElectricCar.getoff()");
-        engine_->stop();
+        getEngine()->stop();
     }
 };
 
@@ -80,6 +106,11 @@ int test_ElectricCar() {
         return EXIT_FAILURE;
     }
 }
+
+/**
+ * Strategy パターンとの違いがいまひとつ分かりづらいかな。
+ * 自動車の基底クラスができて、少しだけ Strategy との違いが出た。
+*/
 
 int main(void) {
     puts("START Bridge パターン ===");
