6af5c4bf34467769bae18520c97fbcf8a882f46f
Jack <alienm14xr2@gmail.com>
2024-01-09 20:23:44 +0900

    C++ ソフトウェア設計 - 7 章 Bridge パターン、Prototype パターン、External Polymorphism パターン。 - サンプルの実装を開始した。 - 未テスト。

--- C++-Software-Design/src/chapter_7_bridge.cpp
+++ C++-Software-Design/src/chapter_7_bridge.cpp
@@ -7,6 +7,7 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_7_bridge.cpp -o ../bin/main
 */
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
@@ -14,6 +15,50 @@ template <class M, class D>
 void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
     cout << "DEBUG: " << message << '\t' << debug << endl;
 };
+
+/**
+ * サンプルは ElectricCar とその Engine で説明している。
+*/
+
+/**
+ * エンジン 基底クラス
+*/
+class Engine {
+public:
+    virtual ~Engine() = default;
+    virtual void start() = 0;
+    virtual void stop() = 0;
+    // more engine-specific functions.
+private:
+    // ...
+};
+
+class ElectricEngine final : public Engine {
+public:
+    void start() override {
+        puts("EE start engine.");
+    }
+    void stop() override {
+        puts("EE stop engine.");
+    }
+private:
+    // ...
+};
+
+/**
+ * 電気自動車クラス
+*/
+class ElectricCar {
+private:
+    std::unique_ptr<Engine> engine_;
+public:
+    explicit ElectricCar(/* mybe some engine arguments */):engine_{std::make_unique<ElectricEngine>(/* engine arguments */)} {}
+    void drive() {
+        puts("--- ElectricCar.drive()");
+    }
+};
+
+
 
 int main(void) {
     puts("START Bridge パターン ===");
