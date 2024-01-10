d57bdee5bba248f8e3351eb56d8d89db168d3766
Jack <alienm14xr2@gmail.com>
2024-01-11 05:43:13 +0900

    C++ ソフトウェア設計 - 7 章 Bridge パターン、Prototype パターン、External Polymorphism パターン。 - Prototype パターンのサンプル実装を開始した。 - 未テスト。

--- C++-Software-Design/src/chapter_7_prototype.cpp
+++ C++-Software-Design/src/chapter_7_prototype.cpp
@@ -3,6 +3,10 @@
  * 
  * ガイドライン 30 ：コピー演算を抽象化するには Prototype パターン
  * 
+ * 何か具体的なものは判っていない状態、C++ で言えば、具象クラスは判らずに抽象クラスのみ判っている状態で
+ * Foo*（Foo は抽象クラス） をコピーするための解として、古典的 GoF デザインパターンの Prototype パターン
+ * がある。
+ * 
  * e.g. compile.
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_7_prototype.cpp -o ../bin/main
 */
@@ -13,6 +17,17 @@ using namespace std;
 template <class M, class D>
 void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
     cout << "DEBUG: " << message << '\t' << debug << endl;
+};
+
+/**
+ * 羊もどきの例：Animal のコピー
+*/
+
+class Animal {
+public:
+    virtual ~Animal() = default;
+    virtual void makeSound() const = 0;
+    // .. more animal-specific functions
 };
 
 int main(void) {
