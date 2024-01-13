23d41d57ea90119e144a5a9d989b1f944053b54c
Jack <alienm14xr2@gmail.com>
2024-01-13 14:16:21 +0900

    C++ ソフトウェア設計 - 8 章 Type Erasure パターン。 - Read を進めた。 - std::any のサンプルを実装した。

--- C++-Software-Design/src/chapter_8_type_erasure.cpp
+++ C++-Software-Design/src/chapter_8_type_erasure.cpp
@@ -24,6 +24,9 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_8_type_erasure.cpp -o ../bin/main
 */
 #include <iostream>
+#include <any>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
@@ -32,11 +35,31 @@ void (*ptr_lambda_debug)(M, D) = [](auto message, auto debug) -> void {
     cout << "DEBUG: " << message << '\t' << debug << endl;
 };
 
+int sample_1() {
+    puts("--- sample_1");
+    try {
+        std::any a;         // create an empty 'any'
+        a = 3;              // Storing an 'int' inside the 'any'
+        a = "some string"s; // Replacing the 'int' with a 'std::string'
+
+        // There is nothing we can do with the 'any' except for getting the value back.（any は値を取り出すことぐらいしか出来ない。）
+        std::string s = std::any_cast<std::string>(a);
+        ptr_lambda_debug<const char*,const string&>("s is ",s);
+        return EXIT_SUCCESS;
+    } catch(exception& e) {
+        cout << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+}
+
 int main(void) {
     puts("START Type Erasure パターンでの継承階層の置換を検討する ===");
     if(0.01) {
         double pi = 3.141592;
         ptr_lambda_debug<const char*,const int&>("pi is ", pi);
+    }
+    if(1.00) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ",sample_1());
     }
     puts("=== Type Erasure パターンでの継承階層の置換を検討する END");
     return 0;
