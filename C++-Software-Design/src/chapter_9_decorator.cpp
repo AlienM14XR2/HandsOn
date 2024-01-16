04dd8d54d11cc741df8b6fbc694f9a239d16a438
Jack <alienm14xr2@gmail.com>
2024-01-16 15:51:04 +0900

    C++ ソフトウェア設計 - 9 章 Decorator パターン。 - Moeny クラスを実装した、Decorator パターンとは直接的な関係のないもの。 - テストを実装した。

--- C++-Software-Design/src/chapter_9_decorator.cpp
+++ C++-Software-Design/src/chapter_9_decorator.cpp
@@ -15,6 +15,7 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_9_decorator.cpp -o ../bin/main
 */
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -28,11 +29,61 @@ void (*ptr_lambda_debug)(Message, Debug) = [](auto message, auto debug) -> void 
  * まずは、Decorator パターンの古典的実装から行う。
 */
 
+class Money {
+private:
+    double amount;      // 額（金額）
+    double price;       // 価格
+    Money(): amount{0}, price{0} {}
+public:
+    Money(const double& _amount): amount(_amount), price{0} 
+    {}
+    Money(const Money& own) {*this = own;}
+    ~Money() {}
+
+    double getAmount() const {
+        return amount;
+    }
+    double getPrice() const {
+        return price;
+    }
+    Money operator*(const double& factor) {
+        price = amount + (amount * factor);
+        return *this;
+    }
+    Money operator+(Money rhs) {
+        amount = amount + rhs.amount;
+        price = price + rhs.price;
+        return *this;
+    }
+};
+
+int test_Money() {
+    puts("--- test_Money");
+    try {
+        Money m1{30}, m2{60};
+        Money m3 = m1 + m2;
+        m3 = m3 * 0.1;
+        double resultAmount = -1;
+        double resultPrice = -1;
+        ptr_lambda_debug<const char*,const double&>("m3 amount is ",resultAmount = m3.getAmount());
+        ptr_lambda_debug<const char*,const double&>("m3 price is ",resultPrice = m3.getPrice());
+        assert(resultAmount == 90);
+        assert(resultPrice == 99);
+        return EXIT_SUCCESS;
+    } catch(exception& e) {
+        cout << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+}
+
 int main(void) {
     puts("START カスタマイズを階層化するには Decorator パターン ===");
     if(0.01) {
         double pi = 3.141592;
         ptr_lambda_debug<const char*,const double&>("pi is ",pi);
+    }
+    if(1.00) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ",test_Money());
     }
     puts("=== カスタマイズを階層化するには Decorator パターン END");
     return 0;
