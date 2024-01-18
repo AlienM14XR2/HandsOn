232799fc5546645114f2c6c86b13339983b3fb58
Jack <alienm14xr2@gmail.com>
2024-01-18 10:13:58 +0900

    C++ ソフトウェア設計 - 9 章 Decorator パターン。 - 値ベースのコンパイル時 Decorator パターン。 - 新規ソースファイル用意した。

---
+++ C++-Software-Design/src/chapter_9_decorator2.cpp
@@ -0,0 +1,30 @@
+/**
+ * 9 章 Decorator パターン
+ * 
+ * ガイドライン 36 ：実行時抽象化とコンパイル時抽象化間のトレードオフを理解する
+ * 
+ * e.g. compile.
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_9_decorator2.cpp -o ../bin/main
+*/
+#include <iostream>
+
+using namespace std;
+
+template <class M, class D>
+void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
+    cout << "DEBUG: " << message << '\t' << debug << endl;
+};
+
+/**
+ * 値ベースのコンパイル時 Decorator パターン
+*/
+
+int main(void) {
+    puts("START Decorator パターン ===");
+    if(0.01) {
+        double pi = 3.141592;
+        ptr_lambda_debug<const char*,const double&>("pi is ",pi);
+    }
+    puts("=== Decorator パターン END");
+    return 0;
+}
