e723711d230963b935f975048664db00c221af08
Jack <alienm14xr2@gmail.com>
2024-01-18 17:57:23 +0900

    C++ ソフトウェア設計 - 9 章 Decorator パターン。 - クラスを一部移植した。 - 未テスト。

--- C++-Software-Design/src/chapter_9_decorator2.cpp
+++ C++-Software-Design/src/chapter_9_decorator2.cpp
@@ -7,6 +7,8 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_9_decorator2.cpp -o ../bin/main
 */
 #include <iostream>
+#include <cassert>
+#include <memory>
 
 using namespace std;
 
@@ -24,6 +26,155 @@ void (*ptr_lambda_debug)(M,D) = [](auto message, auto debug) -> void {
  * することが目的です。
  * ConferenceTicket クラスを例に進めます。
 */
+
+/**
+ * 金額クラス
+*/
+class Money {
+private:
+    double amount;      // 額（金額）
+    // TODO 今回の学習とは全く関係がないが、951.048 等の場合、小数点以下第 3 位を切り上げする方法を考えてみよう。
+    double price;       // 価格
+    Money(): amount{0}, price{0} {}
+public:
+    Money(const double& _amount): amount(_amount), price{0} 
+    {}
+    Money(const Money& own) {*this = own;}
+    ~Money() {}
+
+    double getAmount() const {
+        return amount;
+    }
+    double getPrice() const {
+        return price;
+    }
+    Money operator*(const double& factor) {
+        price = amount + (amount * factor);     // 私の実装では、ここで税率を含めた計算を行っているので、サンプルとは実装が異なる。
+        return *this;
+    }
+    Money operator+(Money rhs) {
+        amount = amount + rhs.amount;
+        price = price + rhs.price;
+        return *this;
+    }
+    Money operator-(Money rhs) {
+        amount = amount - rhs.amount;
+        price = price -rhs.price;
+        return *this;
+    }
+};
+
+int test_Money() {
+    puts("--- test_Money");
+    try {
+        Money m1{30}, m2{60};
+        Money m3 = m1 + m2;
+        m3 = m3 * 0.1;
+        double resultAmount = -1;
+        double resultPrice = -1;
+        ptr_lambda_debug<const char*,const double&>("m3 amount is ",resultAmount = m3.getAmount());
+        ptr_lambda_debug<const char*,const double&>("m3 price is ",resultPrice = m3.getPrice());
+        assert(resultAmount == 90);
+        assert(resultPrice == 99);
+        return EXIT_SUCCESS;
+    } catch(exception& e) {
+        cout << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+}
+
+/**
+ * 商品基底クラス
+*/
+class Item {
+public:
+    virtual ~Item() = default;
+    virtual Money price() const = 0;
+};
+
+/**
+ * 商品拡張クラス
+ * Decorator パターンの肝になるもの。
+*/
+class DecoratedItem : public Item {
+private:
+    std::unique_ptr<Item> item;
+protected:
+    Item&       getItem()       { return *item; }
+    const Item& getItem() const { return *item; }
+public:
+    explicit DecoratedItem(std::unique_ptr<Item> _item) : item(std::move(_item))
+    {
+        if(!item) {
+            throw std::invalid_argument("Invalid item.");
+        }
+    }
+};
+
+/**
+ * C++ 書籍
+ * 商品の具象クラス。
+*/
+class CppBook final : public Item {
+private:
+    std::string title;
+    Money money{0};
+public:
+    explicit CppBook(std::string _title, Money _money) : title{std::move(_title)}, money{std::move(_money)}
+    {}
+
+    const std::string& getTitle() const { return title; }
+    Money price() const override { return money; }
+};
+
+/**
+ * Discounted クラス
+ * Item を指す std::unique_ptr と 値引き率を表す double 型（0.0 から 1.0）で初期化します。
+*/
+class Discounted final : public DecoratedItem {
+private:
+    double factor;
+public:
+    explicit Discounted(double discount, std::unique_ptr<Item> item): DecoratedItem(std::move(item)), factor(discount)     // この初期化順番を逆にはできないから注意が必要。
+    {
+        if( discount < 0.0 || discount > 1.0 ) {
+            ptr_lambda_debug<const char*,const double&>("your input discount is ",discount);
+            throw std::invalid_argument("Invalid discount");
+        }
+    }
+
+    /**
+     * price() 関数こそが、Decorator パターンをフル活用する箇所になる。
+    */
+
+    Money price() const override {
+        double amount = getItem().price().getAmount() - (getItem().price().getAmount() * factor);
+        return Money{amount};
+//        return getItem().price() * factor;
+    }
+};
+
+/**
+ * Taxed クラス
+ * コンストラクタで税率を計算している。
+*/
+class Taxed final : public DecoratedItem {
+private:
+    double factor;
+public:
+    explicit Taxed(double taxRate, std::unique_ptr<Item> item): DecoratedItem(std::move(item)), factor(taxRate)
+    {
+        if( taxRate < 0.0 ) {
+            ptr_lambda_debug<const char*,const double&>("your input taxRate is ",taxRate);
+            throw std::invalid_argument("Invalid tax");
+        }
+    } 
+
+    Money price() const override {
+        return getItem().price() * factor;
+    }
+};
+
 
 int main(void) {
     puts("START Decorator パターン ===");
