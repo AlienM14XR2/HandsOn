f67009cb55e0b5fa292ef79d99397a946feab8ee
Jack <alienm14xr2@gmail.com>
2024-01-23 02:16:16 +0900

    Effective Modern C++ - 1 章 型推論。 - 「ケース 2 ：ParamType がユニバーサル参照である」、T&& ユニバーサル参照の検証。 - テストを実装完了。

--- Effective-Modern-C++/src/chapter_1_type_inference.cpp
+++ Effective-Modern-C++/src/chapter_1_type_inference.cpp
@@ -111,6 +111,62 @@ int test_fp() {
  * 型推論システムは期待通りに動作します。
 */
 
+/**
+ * ケース 2 ：ParamType がユニバーサル参照である
+ * 
+ * ユニバーサル参照を仮引数にとるテンプレートの場合はずっと分かりにくくなります。
+ * この種の仮引数は右辺値参照のように宣言されますが（T を仮引数にとる関数テンプレートでは、ユニバーサル参照に宣言する型が T&&）、
+ * 左辺値の実引数が渡された場合の動作が変化します。
+ * 
+ * e.g. 関数テンプレートの場合。
+ * 
+ * template <class T>
+ * void f(ParamType param)
+ * 
+ * f(expr)                  // call with some expression ... f に式を与え呼び出す。
+ * 
+ * - expr が左辺値ならば、T も ParamType も左辺値参照と推論される。これは 2 つの意味で特殊である。まず、テンプレートの型推論で、
+ *   T を参照として推論するのはこの場合だけである。もう 1 つは、ParamType の宣言には右辺値参照という形態をとりながら、推論される
+ *   型は左辺値参照となる点である。
+ * 
+ * - expr が右辺値の場合は、「通常の」規則が適用される（ケース 1）。
+*/
+
+template <class T>
+void f_uref(T&& param) {        // param is universal reference.
+    puts("------ f_uref");
+    ptr_lambda_debug<const char*,const T&>("param is ", param);
+}
+
+int test_f_uref() {
+    puts("--- test_f_uref");
+    try {
+        int x = 33;
+        const int cx = x;
+        const int& rx = x;      // rx は const int としての x の参照
+        /**
+         * 上記は先の例と同じ。
+        */
+
+        f_uref(x);      // x は左辺値、よって T は int&
+        f_uref(cx);     // cx は左辺値、よって T は const int&, param の型も const int&
+        f_uref(rx);     // rx は左辺値、よって T は const int&, patam の型も const int&
+
+        // 数値リテラルは、y = 3 のように右辺値として扱われるもの。
+        f_uref(33);     // 33 は数値リテラル、33 は右辺値、よって T は int, ゆえに param の型は int&&
+        /**
+         * 上例がなぜこのように動作するかについては、項目 24 で述べます。重要なのは、ユニバーサル参照の仮引数に対する型推論規則は、
+         * 左辺値参照や右辺値参照の仮引数の場合とは異なるという点です。特に、型推論が左辺値実引数と右辺値実引数を区別する点は重要であり、
+         * ユニバーサル参照に限った特殊な規則です。
+        */
+
+        return EXIT_SUCCESS;
+    } catch(exception& e) {
+        cout << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+}
+
 int main(void) {
     puts("START 1 章 型推論 ===");
     if(0.01) {
@@ -121,6 +177,7 @@ int main(void) {
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_f());
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_f2());
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_fp());
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_f_uref());
     }
     puts("=== 1 章 型推論 END");
     return 0;
