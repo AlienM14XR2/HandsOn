bcb27973540099a119fdea31f7b8cab3f3af24b7
Jack <alienm14xr2@gmail.com>
2024-01-23 13:14:56 +0900

    Effective Modern C++ - 1 章 型推論。 - 「ケース 3 ：ParamType がポインタでも参照でもない」（値渡し）。 - テストを実装した。

--- Effective-Modern-C++/src/chapter_1_type_inference.cpp
+++ Effective-Modern-C++/src/chapter_1_type_inference.cpp
@@ -32,6 +32,7 @@ void f(T& param) {      // const なし
     puts("------ f");
     // param += 1;      // コンパイルエラー、 param is read-only reference
     ptr_lambda_debug<const char*,const T&>("param is ",param);
+    ptr_lambda_debug<const char*,const T*>("param addr is ",&param);
 }
 
 int test_f() {
@@ -40,6 +41,10 @@ int test_f() {
         int x = 27;             // x  は int
         const int cx = x;       // cx は const int
         const int& rx = x;      // rx は const int としての x の参照
+
+        ptr_lambda_debug<const char*,const int*>("x addr is ",&x);
+        ptr_lambda_debug<const char*,const int*>("cx addr is ",&cx);
+        ptr_lambda_debug<const char*,const int*>("rx addr is ",&rx);
 
         f(x);                   // T は int, param の型は int&
         f(cx);                  // T は const int, param の型は int&
@@ -56,22 +61,27 @@ int test_f() {
 }
 
 template <class T>
-void f2(const T& param) {       // const あり
-    puts("------ f2");
+void fc(const T& param) {       // const あり
+    puts("------ fc");
     // param += 1;      // コンパイルエラー、 param is read-only reference
     ptr_lambda_debug<const char*,const T&>("param is ",param);
+    ptr_lambda_debug<const char*,const T*>("param addr is ",&param);
 }
 
-int test_f2() {
-    puts("--- test_f2");
+int test_fc() {
+    puts("--- test_fc");
     try {
         int x = 27;             // x  は int
         const int cx = x;       // cx は const int
         const int& rx = x;      // rx は const int としての x の参照
 
-        f2(x);                  // T は int, param の型は const int&
-        f2(cx);                 // T は int, param の型は int&
-        f2(rx);                 // T は int, param の型は int&
+        ptr_lambda_debug<const char*,const int*>("x addr is ",&x);
+        ptr_lambda_debug<const char*,const int*>("cx addr is ",&cx);
+        ptr_lambda_debug<const char*,const int*>("rx addr is ",&rx);
+
+        fc(x);                  // T は int, param の型は const int&
+        fc(cx);                 // T は int, param の型は int&
+        fc(rx);                 // T は int, param の型は int&
 
         /**
          * 先の例と同様に、型推論では rx の参照性は無視されます。
@@ -90,13 +100,17 @@ template <class T>
 void fp(T* param) {
     puts("------ fp");
     ptr_lambda_debug<const char*,const T&>("param is ",*param);
+    ptr_lambda_debug<const char*,const T*>("param addr is ",param);
 }
 
 int test_fp() {
-    puts("--- fp");
+    puts("--- test_fp");
     try {
         int x = 27;             // x  は int
         const int* px = &x;     // 先の例と変わらず、px は const int としての x を指す
+
+        ptr_lambda_debug<const char*,const int*>("x addr is ",&x);
+        ptr_lambda_debug<const char*,const int*>("px addr is ",px);
 
         fp(&x);                 // T は int, param の型は int*
         fp(px);                 // T は const int, param の型は const int*
@@ -138,6 +152,9 @@ template <class T>
 void f_uref(T&& param) {        // param is universal reference.
     puts("------ f_uref");
     ptr_lambda_debug<const char*,const T&>("param is ", param);
+    // これは面白い T&& ユニバーサル参照のアドレスが知りたい場合は void* で修飾してやる。
+    // これは、あくまで私の興味・関心であって、書籍では行っていない。
+    ptr_lambda_debug<const char*,const void*>("param addr is ", &param);
 }
 
 int test_f_uref() {
@@ -149,6 +166,9 @@ int test_f_uref() {
         /**
          * 上記は先の例と同じ。
         */
+        ptr_lambda_debug<const char*,const int*>("x addr is ",&x);
+        ptr_lambda_debug<const char*,const int*>("cx addr is ",&cx);
+        ptr_lambda_debug<const char*,const int*>("rx addr is ",&rx);
 
         f_uref(x);      // x は左辺値、よって T は int&
         f_uref(cx);     // cx は左辺値、よって T は const int&, param の型も const int&
@@ -213,6 +233,7 @@ public:
 
 /**
  * 追加（加算）クラステンプレート
+ * 基底クラス。
 */
 template <class T>
 class AddStrategy {
@@ -260,6 +281,9 @@ int test_PointAddStrategy() {
     }
 }
 
+/**
+ * 数値加算ストラテジ テンプレートクラス
+*/
 template <class Numeric>
 class NumericAddStrategy final : public AddStrategy<Numeric> {
 private:
@@ -272,8 +296,8 @@ public:
     }
 };
 
-int test_NumberAddStrategy() {
-    puts("--- test_NumberAddStrategy");
+int test_NumericAddStrategy() {
+    puts("--- test_NumericAddStrategy");
     try {
         NumericAddStrategy<int> int_strategy{12};
         int int_result = int_strategy.add(9);
@@ -295,6 +319,62 @@ int test_NumberAddStrategy() {
 /**
  * test_PointAddStrategy() と test_NumberAddStrategy() の結果内容を見ると
  * 関数定義の仮引数の型はユニバーサル参照にした方が 数値リテラルを扱えるので良いと言えるのか。
+ * 
+ * 次回以降、本題に戻る。
+*/
+
+/**
+ * ケース 3 ：ParamType がポインタでも参照でもない
+ * 
+ * ParamType がポインタでも参照でもなければ、値渡しとなります。
+ * 
+ * e.g.
+ * template <typename T>
+ * void f(T param);
+ * 
+ * この場合の param は実引数のコピー、すなわちまったく別のオブジェクトとなります。
+ * この param が新規オブジェクトになるという点は、expr から T を推論する動作に大きく影響します。
+ * - これまでと同様に、expr の型が参照であれば、参照性（参照動作部分）は無視される。
+ * - 参照性を無視した expr が const であれば、これも無視する。volatile であれば、同様にこれも無視する
+ *   （volatile オブジェクトは滅多に使用されない。使用されるのは一般にデバイスドライバを実装する場合に限られる。詳細は項目 40 を参照）。
+*/
+
+template <class T>
+void f_by_val(T param) {
+    puts("------ f_by_val");
+    param += 1;     // const 修飾していないので、この式はコンパイルエラーにはならない。
+    ptr_lambda_debug<const char*, const T&>("param is ",param);
+    ptr_lambda_debug<const char*, const T*>("param addr is ",&param);
+}
+
+int test_f_by_val() {
+    puts("--- test_f_by_val");
+    try {
+        int x = 27;
+        const int cx = x;
+        const int& rx = x;
+        /**
+         * 上記は先の例と同じ。
+        */
+
+        ptr_lambda_debug<const char*,const int*>("x addr is ",&x);
+        ptr_lambda_debug<const char*,const int*>("cx addr is ",&cx);
+        ptr_lambda_debug<const char*,const int*>("rx addr is ",&rx);
+
+        f_by_val(x);        // T と param の型はいずれも int
+        f_by_val(cx);       // T と param の型はいずれもやはり int
+        f_by_val(rx);       // T と param の型はいずれもやはり int
+
+        return EXIT_SUCCESS;
+    } catch(exception& e) {
+        cout << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+}
+/**
+ * 実引数が const（および volatile）であっても値渡しの場合のみ無視される点は重要ですので、よく
+ * 覚えておいてください。これまで見てきたように 仮引数が const を指すポインタ／参照の場合は、expr
+ * の const 性は型を推論しても失われません。
 */
 
 int main(void) {
@@ -305,11 +385,12 @@ int main(void) {
     }
     if(1.00) {
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_f());
-        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_f2());
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_fc());
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_fp());
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_f_uref());
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_PointAddStrategy());
-        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_NumberAddStrategy());
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_NumericAddStrategy());
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_f_by_val());
     }
     puts("=== 1 章 型推論 END");
     return 0;
