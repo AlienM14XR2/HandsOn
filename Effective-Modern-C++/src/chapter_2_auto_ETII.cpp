814f3ff393d31bd9c9ec1c051f908097a70dd8aa
Jack <alienm14xr2@gmail.com>
2024-01-29 15:01:29 +0900

    Effective Modern C++ - 2 章 auto。 - 項目 6 auto が期待値とは異なる型を推論する場面では ETII を用いる。 - Read を進めた、コメントを追記した。

--- Effective-Modern-C++/src/chapter_2_auto_ETII.cpp
+++ Effective-Modern-C++/src/chapter_2_auto_ETII.cpp
@@ -8,6 +8,19 @@
  * e.g.)
  * auto highPriority = static_cast<bool>(feature(w)[5]);
  * 
+ * 上記に至る経緯。
+ * std::vector<bool>::reference はプロクシクラス（Proxy Class）の一例です。プロクシクラスとは、他の型
+ * の動作の模倣、拡張を目的としたクラスで、さまざまな場面で使用されます。std::vector<bool>::reference は
+ * std::vector<bool> に対する operator[] がヒットの参照を返すように見せかけるものですし、また標準ライブラリ
+ * のスマートポインタ型は raw ポインタを用いたリソース管理を実現するものです。プロクシクラスの使用については
+ * すでに充分確率されており、実際のデザインパターンの「Proxy」は、もう長いことデザインパターンの殿堂にその名
+ * を連ねています。
+ * 一般に、「ユーザに意識させない（invisible）」プロクシクラスは、auto とは上手には協調しません。
+ * 
+ * 重要ポイント
+ * - 「ユーザに意識させない」プロクシ型は、auto に、初期化式の「誤った」を推論させる。
+ * - 明示的型付け初期化子イディオムを用いれば、auto に目的の型を推論させられる。
+ * 
  * e.g.) compile.
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_2_auto_ETII.cpp -o ../bin/main
 */
