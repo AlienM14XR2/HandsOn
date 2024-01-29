63a7908e93a92061cb279536e6ec560f7a96ef18
Jack <alienm14xr2@gmail.com>
2024-01-29 18:41:53 +0900

    Effective Modern C++ - 3 章 現代の C++ への移行。 - 項目 7 ：オブジェクト作成時の () と {} の違い。 - Read を進めた、サンプルを実装した。

--- Effective-Modern-C++/src/chapter_3_modern_c++.cpp
+++ Effective-Modern-C++/src/chapter_3_modern_c++.cpp
@@ -7,6 +7,9 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_3_modern_c++.cpp -o ../bin/main
 */
 #include <iostream>
+#include <vector>
+#include <string>
+#include <atomic>
 
 using namespace std;
 
@@ -59,6 +62,50 @@ void sample2() {
     w3 = w1;            // 代入である。コピー演算 operator= を呼び出す。
 }
 
+/**
+ * C++11 では『初期化の統一記法』を導入しました。
+ * どんな初期化でも表現できる唯一の初期化構文です、少なくとも概念としては、
+ * 初期化の統一記法では『波括弧』を用います。
+*/
+
+template <class T, std::size_t N>
+constexpr std::size_t arraySize(T(&)[N]) noexcept {
+    return N;
+}
+
+void sample3() {
+    puts("--- sample3");
+    std::vector<int> v{1,3,5};
+    for(auto val: v) {
+        ptr_lambda_debug<const char*,const decltype(val)&>("val is ",val);
+    }
+
+    Widget w{};
+    ptr_lambda_debug<const char*,const char*>("w type is ",typeid(w).name());
+
+    std::string names[] {{"Jack"},{"Derek"},{"Alice"}};
+    size_t namesSize = sizeof(names)/sizeof(names[0]);
+    ptr_lambda_debug<const char*,const size_t&>("namesSize is ",namesSize);
+    size_t namesSize2 = arraySize(names);
+    ptr_lambda_debug<const char*,const size_t&>("namesSize2 is ",namesSize2);
+}
+
+/**
+ * コピー不可能なオブジェクトは（std::atomic など。項目 40 を参照）、波括弧でも丸括弧でも
+ * 初期化できますが、「=」では初期化できません。
+*/
+
+void sample4() {
+    puts("--- sample4");
+    std::atomic<int> ai1{3};        // 問題なし
+    std::atomic<int> ai2(6);        // 問題なし
+    std::atomic<int> ai3 = 99;       // エラー ... えっ、エラーにならないね。
+
+    ptr_lambda_debug<const char*,const int&>("ai1 is ",ai1);
+    ptr_lambda_debug<const char*,const int&>("ai2 is ",ai2);
+    ptr_lambda_debug<const char*,const int&>("ai3 is ",ai3);
+}
+
 int main(void) {
     puts("START 項目 7 ：オブジェクト作成時の () と {} の違い ===");
     if(0.01) {
@@ -68,6 +115,8 @@ int main(void) {
     if(1.00) {
         sample();
         sample2();
+        sample3();
+        sample4();
     }
     puts("=== 項目 7 ：オブジェクト作成時の () と {} の違い END");
     return 0;
