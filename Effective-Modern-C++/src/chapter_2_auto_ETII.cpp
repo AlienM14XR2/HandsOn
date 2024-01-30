7a1d77be285cf73c0b4200b9ab092d7d43b3b8af
Jack <alienm14xr2@gmail.com>
2024-01-30 11:18:00 +0900

    Effective Modern C++ - 2 章 auto。 - 項目 6 auto が期待値とは異なる型を推論する場面では ETII を用いる。 - dynamic_cast について個人的な演習、確認を行った。

--- Effective-Modern-C++/src/chapter_2_auto_ETII.cpp
+++ Effective-Modern-C++/src/chapter_2_auto_ETII.cpp
@@ -25,6 +25,7 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_2_auto_ETII.cpp -o ../bin/main
 */
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
@@ -33,11 +34,112 @@ void (*ptr_lambda_debug)(M,D) = [](const auto message, const auto debug) -> void
     cout << "DEBUG: " << message << '\t' << debug << endl;
 };
 
+/**
+ * 次は個人的興味と演習になる。
+ * dynamic_cast について、ダウンキャスト、いいものとはされていないもの。
+*/
+
+class Point {
+private:
+    double x, y;
+public:
+    Point() = delete;
+    Point(const double _x, const double _y): x{_x}, y{_y}
+    {}
+    Point(const Point& own) {*this = own;}
+    ~Point() {}
+    // ...
+    double& getx() noexcept { return x; }
+    double& gety() noexcept { return y; } 
+    pair<double,double> getPoint() noexcept {
+        return pair<double,double>(x, y);
+    }
+};
+
+class Shape {
+public:
+    virtual ~Shape() = default;
+    virtual void draw() = 0; 
+};
+
+class Circle final : public Shape {
+private:
+    Point center{0.0,0.0};
+public:
+    Circle() = delete;
+    Circle(const Point& point): center{std::move(point)}
+    {}
+    Circle(const Circle& own) { *this = own; }
+    ~Circle() {}
+    // ...
+    void draw() override {
+        puts("------ circle draw");
+        ptr_lambda_debug<const char*,const double&>("x is ", center.getx());
+        ptr_lambda_debug<const char*,const double&>("y is ", center.gety());
+    }
+};
+
+class Square final : public Shape {
+private:
+    Point top{0.0, 0.0};
+    Point bottom{30.0, 30.0};
+public:
+    Square() = delete;
+    Square(const Point& _top, const Point& _bottom): top{std::move(_top)}, bottom{std::move(_bottom)}
+    {}
+    Square(const Square& own) { *this = own; }
+    ~Square() {}
+    // ...
+    void draw() override {
+        puts("------ square draw");
+        printf("top(x, y) is (%lf, %lf)\n",top.getx(), top.gety());
+        printf("bottom(x, y) is (%lf, %lf)\n",bottom.getx(), bottom.gety());
+    }
+};
+
+int test_Shape() {
+    puts("--- test_Shape");
+    Shape* shape = nullptr;
+    Circle* circleB = nullptr;
+    try {
+        std::unique_ptr<Shape> shape1 = std::make_unique<Circle>(Circle{Point{30.0, 69.9}});
+        shape1.get()->draw();
+
+        std::unique_ptr<Shape> shape2 = std::make_unique<Square>(Square{Point{0.0, 10.0}, Point{30.0, 33.0}});
+        shape2.get()->draw();
+
+        shape = new Circle(Point{3.0, 6.0});
+        shape->draw();
+
+        Circle circleA = dynamic_cast<Circle&>(*shape);
+        circleA.draw();
+
+        circleB = dynamic_cast<Circle*>(shape);
+        circleB->draw();
+        if(shape) {
+            ptr_lambda_debug<const char*,const Shape*>("shape addr is ", shape);
+            ptr_lambda_debug<const char*,const Shape*>("circleB addr is ", circleB);
+            ptr_lambda_debug<const char*,const Shape*>("circleA addr is ", &circleA);
+        }
+
+        // delete circleB;  // これは上記のデバッグを見れば明らかだけど、shape と全く同じものを指している。なので、これは二重開放につながると。
+        delete shape;
+        return EXIT_SUCCESS;
+    } catch(exception& e) {
+        if(shape) { delete shape; }
+        cout << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+}
+
 int main(void) {
     puts("START 項目 6 auto が期待値とは異なる型を推論する場面では ETII を用いる ===");
     if(0.01) {
         auto pi = 3.141592;
         ptr_lambda_debug<const char*,const decltype(pi)&>("pi is ",pi);
+    }
+    if(2.00) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ",test_Shape());
     }
     puts("=== 項目 6 auto が期待値とは異なる型を推論する場面では ETII を用いる END");
     return 0;
