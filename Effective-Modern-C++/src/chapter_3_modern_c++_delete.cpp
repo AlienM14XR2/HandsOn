704f098ff8962b62c12fad0e42ae39f01084a89d
Jack <alienm14xr2@gmail.com>
2024-01-31 22:20:24 +0900

    Effective Modern C++ - 3 章 現代の C++ への移行。 - 項目 11 ：未定義 private 関数よりも =delete を優先する。 - Read を進めた、新規ソースファイルを用意した。

---
+++ Effective-Modern-C++/src/chapter_3_modern_c++_delete.cpp
@@ -0,0 +1,41 @@
+/**
+ * 3 章 現代の C++ への移行
+ * 
+ * 項目 11 ：未定義 private 関数よりも =delete を優先する
+ * 
+ * e.g. compile.
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_3_modern_c++_delete.cpp -o ../bin/main
+*/
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+template <class M, class D>
+void (*ptr_lambda_debug)(M,D) = [](const auto message, const auto debug) -> void {
+    cout << "DEBUG: " << message << '\t' << debug << endl;
+};
+
+int test_debug() {
+    puts("--- test_debug");
+    try {
+        auto pi = 3.141592;
+        ptr_lambda_debug<const char*,const decltype(pi)&>("pi is ", pi);
+        ptr_lambda_debug<const char*,const decltype(pi)*>("pi addr is ", &pi);
+        return EXIT_SUCCESS;
+    } catch(exception& e) {
+        cout << "ERROR: " << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+}
+
+int main() {
+    puts("START 項目 11 ：未定義 private 関数よりも =delete を優先する ===");
+    if(0.01) {
+        int ret = -1;
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ",ret = test_debug());
+        assert(ret == 0);
+    }
+    puts("=== 項目 11 ：未定義 private 関数よりも =delete を優先する END");
+    return 0;
+}
