ec1649057ac5a1ce066f6f338e0a0b063aef718c
Jack <alienm14xr2@gmail.com>
2024-02-01 18:18:10 +0900

    Effective Modern C++ - 3 章 現代の C++ への移行。 - 項目 12 ：オーバーライドする関数は override と宣言する。 - Read を進めた、サンプルを実装し、コメントを追記した。

--- Effective-Modern-C++/src/chapter_3_modern_c++_override.cpp
+++ Effective-Modern-C++/src/chapter_3_modern_c++_override.cpp
@@ -3,12 +3,17 @@
  * 
  * 項目 12 ：オーバーライドする関数は override と宣言する
  * 
+ * 重要ポイント
+ * - オーバーライド関数は override と宣言する。
+ * - メンバ関数の参照修飾子を用いると、左辺値オブジェクト、右辺値オブジェクト（*this）を区別できる。
+ * 
  * e.g. compile
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_3_modern_c++_override.cpp -o ../bin/main
 */
 #include <iostream>
 #include <cassert>
 #include <memory>
+#include <vector>
 
 using namespace std;
 
@@ -87,6 +92,9 @@ Widget makeWidget() {
  * メンバ関数の参照修飾子とは、その関数を左辺値のみに使用する。または右辺値のみに使用するよう制限を課すものです。
  * 参照修飾子は仮想関数か否かとは独立に指定できます。
  * 
+ * 意図しない関数定義にならないように、コンパイラに正しく警告やエラーメッセージを出力させるためにも派生クラスのオーバーライド関数には
+ * override を付けること。
+ * 
 */
 
 int test_Derived() {
@@ -115,6 +123,83 @@ int test_Widget() {
     }
 }
 
+/**
+ * メンバ関数の参照修飾子についてはまだあります。
+ * 実引数に左辺値のみをとる関数を記述する場合、非 const な左辺値参照仮引数とするのが通例です。
+ * 
+ * void doSomething(Widget& w);     // 左辺値の Widget のみをとる
+ * 
+ * また、右辺値のみをとる関数を記述する場合、右辺値参照仮引数とするのが通例です。
+ * 
+ * void doSomething(Widget&& w);    // 右辺値の Widget のみをとる
+ * 
+ * メンバ関数の参照修飾子は、そのメンバ関数を実行するオブジェクト、すなわち *this を同様に区別
+ * するためのものです。メンバ関数宣言の末尾に記述し、そのメンバ関数を実行するオブジェクト（*this）
+ * が const であることを表す、const とよく似ています。
+ * 参照修飾子が必要になるメンバ関数は多くありませんが、出会うこともあるでしょう。例えば、Widget 
+ * クラスが std::vector のメンバ変数をもっており、コード利用者が直接アクセスできるようアクセサ関数
+ * を提供する場合を考えます。
+*/
+
+class WidgetV2 {
+public:
+    using DataType = std::vector<double>;
+    DataType& data() & noexcept {       // WidgetV2 が左辺値ならば、左辺値を返す 
+        puts("------ WidgetV2::data &");
+        return values; 
+    }                 
+    DataType data() && noexcept {       // WidgetV2 が右辺値ならば、右辺値を返す 
+        puts("------ WidgetV2::data &&");
+        return std::move(values); 
+    }
+private:
+    DataType values;
+};
+
+WidgetV2 makeWidgetV2() {
+    return WidgetV2{};
+}
+
+/**
+ * 上例はカプセル化が高度に図られておらず、まず陽の目を見ることがない設計ですが、それは
+ * 置いといて、コード利用側はどうなるか考えてみましょう。
+*/
+
+int test_WidgetV2() {
+    puts("--- test_WidgetV2");
+    try {
+        WidgetV2 w;
+        auto vals1 = w.data();      // w.values を vals1 へコピー
+        /**
+         * WidgetV2::data の戻り型は左辺値参照であり（厳密に言えば std::vector<double>&）、左辺値参照は
+         * 左辺値となるように定義されているため、vals1 を左辺値から初期化することになります。そのため、
+         * コメントにもあるように、vals1 は w.values からコピーコンストラクトされます。
+         * ここで WidgetV2 を作成する factory 関数があるとしましょう。
+        */
+
+        auto vals2 = makeWidgetV2().data();     // Widget 内の値を vals2 へコピー
+
+        /**
+         * 上例でも WidgetV2::data は左辺値参照を返し、左辺値参照は同様に左辺値になるため、やはり
+         * 新規オブジェクト（vals2）はWidgetV2 内の values からコピーコンストラクトされます。しかし、
+         * 今度の WidgetV2 は makeWidgetV2 が返した一時オブジェクトのため（すなわち右辺値）、内部で
+         * 実行される std::vector のコピーは時間の無駄です。ムーブの方が好ましい動作ですが、data は
+         * 左辺値参照を返すため、C++ の規則ではコンパイラがコピーするコードを生成することとしています
+         * （「as if rule」による最適化の余地はありますが、コンパイラにそこまで期待してはいけません ）。
+         * ここで必要なのは、右辺値 WidgetV2 の data 呼び出しでは、結果も右辺値となることを明示する
+         * 方法です。参照修飾子を用いることで、左辺値、右辺値どちらものオーバーロードも実現できます。
+         * 
+         * 一時オブジェクトのようなコピーの内部的な無駄を無くす、高速化に貢献する可能性がある場合に
+         * 右辺値参照を用いる、私には今はそのような解釈だな。
+        */
+
+        return EXIT_SUCCESS;
+    } catch(exception& e) {
+        cout << "ERROR: " << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+}
+
 int main(void) {
     puts("START 項目 12 ：オーバーライドする関数は override と宣言する ===");
     if(0.01) {
@@ -125,6 +210,9 @@ int main(void) {
     if(1.00) {
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_Derived());
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_Widget());
+    }
+    if(1.01) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_WidgetV2());
     }
     puts("=== 項目 12 ：オーバーライドする関数は override と宣言する END");
     return 0;
