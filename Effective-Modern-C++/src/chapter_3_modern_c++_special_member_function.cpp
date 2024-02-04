b4b678397cbbb2af2e66af71c215a941b24b2cb7
Jack <alienm14xr2@gmail.com>
2024-02-04 19:16:43 +0900

    Effective Modern C++ - 3 章 現代の C++ への移行。 - 項目 17 ：自動的に生成される特殊メンバ関数を理解する。 - コメントを追記しサンプルを実装した。

--- Effective-Modern-C++/src/chapter_3_modern_c++_special_member_function.cpp
+++ Effective-Modern-C++/src/chapter_3_modern_c++_special_member_function.cpp
@@ -60,12 +60,73 @@ int test_debug() {
  * それぞれのシグネチャを挙げます。
 */
 
+class Point {
+private:
+    double x,y;
+public:
+    Point() = delete;
+    Point(double _x, double _y): x{_x}, y{_y}
+    {}
+    Point(const Point& own) = default;
+    Point& operator=(const Point& lhs) = default;
+    ~Point() {}
+    // ...
+    double getx() noexcept { return x; }
+    double gety() noexcept { return y; }
+
+};
+class Widget {
+private:
+    Point top{-10.0, 0.0}, bottom{-5.0, 5.0};
+public:
+    Widget(Point _top, Point _bottom): top{std::move(_top)}, bottom{std::move(_bottom)}                      // デフォルトコンストラクタ
+    {}
+    Widget(const Widget&) = default;        // デフォルトコピーコンストラクタ
+    Widget(Widget&& rhs) {                  // ムーブコンストラクタ
+        puts("------ move constructor");
+        *this = rhs;
+    }
+    Widget& operator=(Widget&& rhs) {       // ムーブ代入演算子
+        puts("------ move assignment operator");
+        *this = rhs; 
+        return *this; 
+    }
+    Widget& operator=(const Widget&) = default;
+    // ...
+    Point& getTop()    noexcept { return top; }
+    Point& getBottom() noexcept { return bottom; }
+};
+
+int test_Widget() {
+    puts("--- test_Widget");
+    try {
+        Widget widget{Point{0.0, 0.0}, Point{30.0, 60.0}};
+        Widget w1 = std::move(widget);      // ムーブコンストラクタが呼ばれる。
+        Widget w2 = Widget{Point{0.0, 0.0}, Point{33.3, 66.6}};
+        w2 = std::move(widget);             // ムーブ代入演算子が呼ばれる。
+
+        printf("w1 top(x,y) : bottom(x,y) is top(%.2f,%.2f): bottom(%.2f,%.2f)\n", w1.getTop().getx(), w1.getTop().gety(), w1.getBottom().getx(), w1.getBottom().gety());
+        printf("w2 top(x,y) : bottom(x,y) is top(%.2f,%.2f): bottom(%.2f,%.2f)\n", w2.getTop().getx(), w2.getTop().gety(), w2.getBottom().getx(), w2.getBottom().gety());
+
+        ptr_lambda_debug<const char*,const Widget*>("widget addr is ", &widget);
+        ptr_lambda_debug<const char*,const Widget*>("w1     addr is ", &w1);
+        ptr_lambda_debug<const char*,const Widget*>("w2     addr is ", &w2);
+        return EXIT_SUCCESS;
+    } catch(exception& e) {
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
 int main(void) {
     puts("START 項目 17 ：自動的に生成される特殊メンバ関数を理解する ===");
     if(0.01) {
         int ret1 = -1;
         ptr_lambda_debug<const char*, const int&>("Play and Result ... ", ret1 = test_debug());
         assert(ret1 == 1);      // 意図的に Error を発生させているため
+    }
+    if(1.00) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_Widget());
     }
 
     puts("=== 項目 17 ：自動的に生成される特殊メンバ関数を理解する END");
