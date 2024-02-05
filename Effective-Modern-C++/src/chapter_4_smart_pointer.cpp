8f9d74ac27a5290dfa86304819c766063fdcce7e
Jack <alienm14xr2@gmail.com>
2024-02-05 23:15:02 +0900

    Effective Modern C++ - 4 章 スマートポインタ。 - 項目 18 ：独占するリソースの管理には std::unique_ptr を用いる。 - コメントを追記した。

--- Effective-Modern-C++/src/chapter_4_smart_pointer.cpp
+++ Effective-Modern-C++/src/chapter_4_smart_pointer.cpp
@@ -22,6 +22,27 @@
  * 6. ポインタが不正か否かを通知する術が、一般に存在しない。不正ポインタとは、オブジェクトを保存
  *    していないメモリを指すポインタであり、ポインタが指しているオブジェクトを破棄すると発生する。
  * 
+ * C++11 には 4 種類のスマートポインタがあります。
+ * std::auto_ptr
+ * std::unique_ptr
+ * std::shared_ptr
+ * std::weak_ptr
+ * いずれもダイナミックに割り当てたオブジェクトのライフサイクルの管理を支援するべく設計されたものです。
+ * すなわち、適切な時に、（例外発生時も含め）、適切な方法でオブジェクトを確実に破棄し、リソースの解放
+ * 漏れを防止します。
+ * 
+ * 項目 18 ：独占するリソースの管理には std::unique_ptr を用いる
+ * 
+ * std::unique_ptr は独占所有（排他的所有権）セマンティクスを備えます。非ヌルの std::unique_ptr はその
+ * 指す対象を常に所有し、std::unique_ptr をムーブすると、所有権も元のポインタからムーブ先のポインタへ
+ * 転送されます（元のポインタはヌルになる）。std::unique_ptr はコピーできません。std::unique_ptr は
+ * 『ムーブ専用型』です。非ヌルの std::unique_ptr を破棄すると、std::unique_ptr が対象のリソースを破棄
+ * します。デフォルトのリソース破棄では、std::unique_ptr が持つ raw ポインタに対する delete が実行されます。
+ * 
+ * 一般的な使われ方は、factory 関数の戻り値型です。基底クラスのポインタを利用する場合です（実質は各派生クラスのインスタンス）。
+ * デフォルトでは、delete により破棄されますが、カスタムデリータを用いるよう、std::unique_ptr 作成時に指定可能です。
+ * 
+ * 
  * e.g. compile.
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_4_smart_pointer.cpp -o ../bin/main
 */
@@ -59,11 +80,17 @@ int test_debug() {
     }
 }
 
+/**
+ * factory 関数の例を具体的に実装する演習でもやってみる。
+ * Investment 投資を基底クラスとした次の派生クラスを作るものとする。
+ * Stock 株式、Bond 債権、RealEstate 不動産。
+*/
+
 int main(void) {
-    puts("START 4 章 スマートポインタ ===");
+    puts("START 項目 18 ：独占するリソースの管理には std::unique_ptr を用いる ===");
     if(0.01) {
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_debug());
     }
-    puts("=== 4 章 スマートポインタ END");
+    puts("=== 4 章 項目 18 ：独占するリソースの管理には std::unique_ptr を用いる END");
     return 0;
 }
