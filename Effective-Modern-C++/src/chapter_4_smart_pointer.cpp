45f9541e00363afec29788d29bfcd4d38df1b069
Jack <alienm14xr2@gmail.com>
2024-02-05 18:36:36 +0900

    Effective Modern C++ - 4 章 スマートポインタ。 - Read を進めた。 - 新規ソースファイルを用意した。

---
+++ Effective-Modern-C++/src/chapter_4_smart_pointer.cpp
@@ -0,0 +1,48 @@
+/**
+ * 4 章 スマートポインタ
+ * 
+ * e.g. compile.
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_4_smart_pointer.cpp -o ../bin/main
+*/
+#include <iostream>
+
+using namespace std;
+
+template <class M, class D>
+void (*ptr_lambda_debug)(M, D) = [](const auto message, const auto debug) -> void {
+    cout << "DEBUG: " << message << '\t' << debug << endl;
+};
+
+template<class Error>
+concept Reasonable = requires(Error& e) {
+    e.what();
+};
+template<class Error>
+requires Reasonable <Error>
+void (*ptr_print_error)(Error) = [](const auto e) -> void {
+    cout << "ERROR: " << e.what() << endl;
+};
+
+int test_debug() {
+    puts("--- test_debug");
+    try {
+        auto pi = 3.141592;
+        ptr_lambda_debug<const char*,const decltype(pi)&>("pi is ", pi);
+        ptr_lambda_debug<const char*,const decltype(pi)*>("pi addr is ", &pi);
+        throw runtime_error("It's test error.");
+        return EXIT_SUCCESS;
+    } catch(exception& e) {
+        printf("e type is ... %s\n", typeid(e).name());
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
+int main(void) {
+    puts("START 4 章 スマートポインタ ===");
+    if(0.01) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_debug());
+    }
+    puts("=== 4 章 スマートポインタ END");
+    return 0;
+}
