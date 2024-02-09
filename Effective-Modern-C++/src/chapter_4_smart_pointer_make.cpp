369a4b23b57979d954c67d9c438b346d763b590a
Jack <alienm14xr2@gmail.com>
2024-02-09 22:18:28 +0900

    Effective Modern C++ - 4 章 スマートポインタ。 - 項目 21 ：new の直接使用よりも std::make_unique や std::make_shared を優先する。 - 新規ソースファイル作成。

---
+++ Effective-Modern-C++/src/chapter_4_smart_pointer_make.cpp
@@ -0,0 +1,49 @@
+/**
+ * 4 章 スマートポインタ
+ * 
+ * 項目 21 ：new の直接使用よりも std::make_unique や std::make_shared を優先する
+ * 
+ * e.g. compile.
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_4_smart_pointer_make.cpp -o ../bin/main
+*/
+#include <iostream>
+
+template <class M, class D>
+void (*ptr_lambda_debug)(M,D) = [](const auto message, const auto debug) -> void {
+    std::cout << "DEBUG: " << message << '\t' << debug << std::endl;
+};
+
+template <class Error>
+concept Reasonable = requires(Error& e) {
+    e.what();
+};
+
+template <class Error>
+requires Reasonable<Error>
+void (*ptr_print_error)(Error) = [](const auto e) -> void {
+    std::cout << "ERROR: " << e.what() << std::endl;
+};
+
+int test_debug() {
+    puts("=== test_debug");
+    try {
+        auto pi = 3.141592;
+        ptr_lambda_debug<const char*,const decltype(pi)&>("pi is ", pi);
+        ptr_lambda_debug<const char*,const decltype(pi)*>("pi addr is ", &pi);
+        throw std::runtime_error("It's test error.");
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        printf("e type is ... %s\n", typeid(e).name());
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
+int main(void) {
+    puts("START 項目 21 ：new の直接使用よりも std::make_unique や std::make_shared を優先する ===");
+    if(0.01) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_debug());
+    }
+    puts("=== 項目 21 ：new の直接使用よりも std::make_unique や std::make_shared を優先する END");
+    return 0;
+}
