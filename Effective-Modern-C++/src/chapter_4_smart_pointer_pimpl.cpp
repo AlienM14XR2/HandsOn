ff92be79cc8deba10a4d833e80dfc2a17f2cea50
Jack <alienm14xr2@gmail.com>
2024-02-11 00:01:46 +0900

    Effective Modern C++ - 4 章 スマートポインタ。 - 項目 22 ：Pimpl イディオムを用いる際は特殊メンバ関数を定義する。 - サンプルを実装した。

--- Effective-Modern-C++/src/chapter_4_smart_pointer_pimpl.cpp
+++ Effective-Modern-C++/src/chapter_4_smart_pointer_pimpl.cpp
@@ -14,6 +14,7 @@
 */
 #include <iostream>
 #include <vector>
+#include <memory>
 
 template <class M, class D>
 void (*ptr_lambda_debug)(M, D) = [](const auto message, const auto debug) -> void {
@@ -73,15 +74,6 @@ class Gadget {
 
 };
 
-/**
- * 本来はヘッダとソースが分かれるので次のようになると考えてください。
- * 
- * #include "widget.h"
- * #include "gadget.h"
- * #include <string>
- * #include <vector>
-*/
-
 // Widget 宣言 widget.h に記述すべきこと
 class Widget {
 private:
@@ -91,6 +83,15 @@ public:
     Widget();
     ~Widget();
 };
+
+/**
+ * 本来はヘッダとソースが分かれるので次のようになると考えてください。
+ * 
+ * #include "widget.h"
+ * #include "gadget.h"
+ * #include <string>
+ * #include <vector>
+*/
 
 // Widget 定義 widget.cpp
 struct Widget::Impl {
@@ -105,23 +106,129 @@ Widget::~Widget() {
     delete pImpl;
 }
 
+/**
+ * これで、Widget は std::string std::vecotor Gadget を直接使用しなくなったため、これらのヘッダを Widget の利用者が #include する
+ * 必要もなくなりました。コンパイル時間を短縮できるばかりか、これらの型のヘッダに何らかの変更が加えられても、Widget 利用者は影響を
+ * 受けません。
+ * 宣言はしたけど定義をしない型を指し、『不完全型』と言います。上例の Widget::Impl はまさに不完全型です。 不完全型に対し直接できる
+ * ことはほどんどありませんが、そのポインタは宣言できます。Pimpl イディオムではこの点を活用しています。
+ * Pimpl イディオムの第一部は、不完全型を指すポインタをメンバ変数として宣言することです。第二部は、元クラスにあったメンバ変数を保持
+ * するオブジェクトの、ダイナミックな割当／解放です。割当／解放コードはヘッダファイルではなく、実装を記述するソースコードに記述します。
+ * 
+ * 上例では std::string std::vector Gadget の各ヘッダには変わらず依存していることを示すために、敢えて #include ディレクティブを残して
+ * あります。しかし、この依存性は widget.h から widget.cpp へ移動しています（Widget を利用するものからは見えない）。Pimpl オブジェクト
+ * の割当／解放は忘れてはいけません。
+ * 
+ * 本章の主眼は raw ポインタよりスマートポインタの方が望ましいと示すことにあります。したがって、次のようになります。
+*/
+
+// Widget 宣言 widget.h に記述すべきこと
+class WidgetV2 {
+private:
+    struct Impl;
+    std::unique_ptr<Impl> pImpl;
+public:
+    WidgetV2();
+    // 私の環境では次の定義は不要だった。そもそもソースにすべて記述しているため。
+    ~WidgetV2();
+    // デストラクタを明示した際は、ムーブの特殊メンバ関数の自動生成が抑制される、利用する場合は明示しないといけない。
+    WidgetV2(WidgetV2&& rhs);
+    WidgetV2& operator=(WidgetV2&& rhs);
+    // スマートポインタはコピーではなくムーブを利用する、ムーブ専用型。
+
+    /**
+     * 必要があれば、コピー演算もヘッダで宣言し、ソースで定義する
+    */
+    WidgetV2(const WidgetV2& own);
+    WidgetV2 operator=(const WidgetV2& own);
+
+    // ...
+    std::unique_ptr<Impl>& getResource() {
+        return pImpl;
+    }
+};
 
 /**
- * ソースファイルとヘッダファイルを分けた書き方では次の定義になる。
+ * 本来はヘッダとソースが分かれるので次のようになると考えてください。
  * 
-struct Widget::Impl {
+ * #include "widget.h"
+ * #include "gadget.h"
+ * #include <string>
+ * #include <vector>
+*/
+
+// Widget 定義 widget.cpp
+struct WidgetV2::Impl {
     std::string name;
     std::vector<double> data;
     Gadget g1, g2 ,g3;
 };
+
+WidgetV2::WidgetV2() : pImpl(std::make_unique<Impl>())
+{
+    ptr_lambda_debug<const char*,const char*>("pImpl type is ", typeid(pImpl).name());
+    ptr_lambda_debug<const char*,const char*>("*pImpl type is ", typeid(*pImpl).name());
+    ptr_lambda_debug<const char*,Impl*>("*pImpl addr is ", pImpl.get());
+    ptr_lambda_debug<const char*,Impl*>("*pImpl addr is ", &(*pImpl));
+    // pImpl.get()->data;
+    // (*pImpl).data;
+}
+// 解放は std::uniqu_ptr が行う。
+// WidgetV2::~Widget() {
+//     delete pImpl;
+// }
+
+// 私の環境では次の定義は不要だった。そもそもソースにすべて記述しているため。
+WidgetV2::~WidgetV2() = default;
+WidgetV2::WidgetV2(WidgetV2&& rhs) = default;
+WidgetV2& WidgetV2::operator=(WidgetV2&& rhs) = default;
+WidgetV2::WidgetV2(const WidgetV2& own) //: pImpl(std::make_unique<Impl>(*own.pImpl))
+{
+    if(own.pImpl.get()) {
+        pImpl = std::make_unique<Impl>(*own.pImpl);
+    }
+}
+// コピー代入演算の実装が書籍通りでは無理だった、そもそもこれはいらないし止めたい。
+WidgetV2 WidgetV2::operator=(const WidgetV2& own) 
+{
+    if(own.pImpl.get()) {
+        pImpl.release();
+        *pImpl = *own.pImpl;
+    }
+    return *this;
+}
+
+/**
+ * 上記コピー演算を眺め（観察し）て、正して置かなければいけない問題が私にはある。
+ * スマートポインタは ポインタ型なのか オプジェクト型なのか、何を言っているかわからないだろうが
+ * 私の今の疑問だ、これをはっきりさせないと、スマートポインタを正しく扱えない。
  * 
+ * アスタリスクを付けるとスマートポインタに指定したオブジェクトが取り出せる。
+ * なしでは、スマートポインタ型。
+ * 納得した、上記のコピー演算では中身（Impl）を取り出している。
 */
 
+int test_WidgetV2() {
+    puts("=== test_WidgetV2");
+    try {
+        WidgetV2 w;
+        auto pImpl = std::move(w.getResource());
+        ptr_lambda_debug<const char*,const char*>("pImpl type is ", typeid(pImpl).name());
+        ptr_lambda_debug<const char*,const char*>("*pImpl type is ", typeid(*pImpl).name());
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
 
 int main(void) {
     puts("START 項目 22 ：Pimpl イディオムを用いる際は特殊メンバ関数を定義する ===");
     if(0.01) {
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_debug());
+    }
+    if(1.00) {
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_WidgetV2());
     }
     puts("=== 項目 22 ：Pimpl イディオムを用いる際は特殊メンバ関数を定義する END");
     return 0;
