d7e7a4b6cff91c6b46d3db4eef8d397803a29461
Jack <alienm14xr2@gmail.com>
2024-02-12 18:53:42 +0900

    Effective Modern C++ - 4 章 スマートポインタ。 - 項目 24 ：ユニバーサル参照と右辺値参照の違い。 - 転送参照のサンプルを実装しコメントを追記した。

--- Effective-Modern-C++/src/chapter_5_move_forward_24.cpp
+++ Effective-Modern-C++/src/chapter_5_move_forward_24.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <chrono>
 #include <ctime>
+#include <vector>
 
 template <class M, class D>
 void (*ptr_lambda_debug)(M,D) = [](const auto message, const auto debug) -> void {
@@ -28,7 +29,6 @@ void (*ptr_print_error)(Error) = [](const auto e) -> void {
 
 void (*ptr_print_now)(void) = [](void) -> void {
     auto now = std::chrono::system_clock::now();
-    // makeLogEntry("Calling 'process'",now);
     std::time_t t = std::chrono::system_clock::to_time_t(now);
     std::cout << std::ctime(&t) << std::endl;
 };
@@ -49,10 +49,68 @@ int test_debug_and_error() {
     }
 }
 
+/**
+ * ある型 T の右辺値参照を宣言するには T&& と記述します。このためソースコードに「T&&」と記述されているのを見れば、これは
+ * 右辺値参照だと受け取れるのは当然に思えます。しかし、あぁなんとしたことか、事はそう簡単ではありません。
+*/
+
+class Widget {
+public:
+    Widget(): name{""}
+    {}
+    Widget(const std::string& _name): name{_name}
+    {}
+    // ...
+    std::string getName() noexcept {
+        return name;
+    }
+private:
+    std::string name;
+};
+
+void foo(Widget&& param) {              // 右辺値参照
+    puts("--- foo");
+}
+
+template<class T>
+void bar(std::vector<T>&& param) {      // 右辺値参照
+    puts("--- bar");
+}
+
+template <class T>
+void buzz(T&& param) {                  // 右辺値参照ではない
+    puts("--- buzz");
+}
+
+void sample() {
+    puts("=== sample");
+    Widget&& w1 = Widget("Chasire");    // 右辺値参照
+
+    auto&& w2 = w1;                     // 右辺値参照ではない
+    // Widget&& w3 = w1;                   // これだとコンパイルエラーになる：）
+    ptr_lambda_debug<const char*,const std::string&>("(w2) name is ", w2.getName());
+}
+
+/**
+ * 実際には「T&&」には 2 つの意味があります。1 つはもちろん右辺値参照です。この参照は期待通りに動作し、バインドするのは
+ * 右辺値のみであり、その『レゾンデトル（存在理由）』はムーブ元になれるオブジェクトの表現にあります。
+ * 「T&&」が持つもう 1 つの意味は、右辺値参照か左辺値参照の『どちらか』一方を表すことです。ソースコード上では右辺値参照
+ * と見分けが付きませんが（どちらも「T&&」）、あたかもあたかも左辺値参照であるかのように振る舞えます（すなわち「T&」）。
+ * この 2 つの性質を併せ持つため、（右辺値参照のように）右辺値にバインド可能であり、また（左辺値参照のように）左辺値にも
+ * バインド可能です。
+ * さらに、const オブジェクト、非 const オブジェクト、volatile オブジェクト、非 volatile オブジェクトにも、また、const 
+ * かつ volatile なオブジェクトにもバインド可能です。実質的に『どんなもの』にもバインド可能です。このような前例のない
+ * 柔軟性を備えた参照には専用の名前が相応しいでしょう。著者は『ユニバーサル参照』と呼んでいます。
+ * ※『転送参照（forwarding reference）』と一般には呼ばれる。
+*/
+
 int main(void) {
     puts("START 項目 24 ：ユニバーサル参照と右辺値参照の違い ===");
     if(0.01) {
         ptr_lambda_debug<const char*,const int&>("Play and Result ... ", test_debug_and_error());
+    }
+    if(1.00) {
+        sample();
     }
     puts("=== 項目 24 ：ユニバーサル参照と右辺値参照の違い END");
 }
