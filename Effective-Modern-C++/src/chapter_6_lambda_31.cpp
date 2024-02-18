194e5047d7fb24512db1eb47c93c1887915f1d51
Jack <alienm14xr2@gmail.com>
2024-02-18 15:20:49 +0900

    Effective Modern C++ - 6 章 ラムダ式。 - 項目 31 ：デフォルトのキャプチャモードは避ける。 - サンプル作成。

--- Effective-Modern-C++/src/chapter_6_lambda_31.cpp
+++ Effective-Modern-C++/src/chapter_6_lambda_31.cpp
@@ -19,8 +19,59 @@
 */
 #include <iostream>
 
+template <class M, class D>
+void (*ptr_lambda_debug)(M,D) = [](const auto message, const auto debug) -> void {
+    std::cout << "DEBUG: " << message << '\t' << debug << std::endl;
+};
+
+template <class Error>
+concept ErrReasonable = requires(Error& e) {
+    e.what();
+};
+template <class Error>
+requires ErrReasonable<Error>
+void (*ptr_print_error)(Error) = [](const auto e) -> void {
+    std::cout << "ERROR: " << e.what() << std::endl; 
+};
+
+int test_debug_and_error() {
+    puts("=== test_debug_and_error");
+    try {
+        // ptr_print_now();
+        auto pi = 3.141592;
+        ptr_lambda_debug<const char*,const decltype(pi)&>("pi is ", pi);
+        ptr_lambda_debug<const char*,const decltype(pi)*>("pi addr is ", &pi);
+        throw std::runtime_error("It's test error.");
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        printf("e type is ... %s\n", typeid(e).name());
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
+void sample() {
+    puts("=== sample");
+    int x = 3;
+
+    auto c1 = [x](int y) {return x * y > 55;};
+    auto c2 = c1;
+    auto c3 = c2;
+
+    auto ret3 = c3(100);
+    ptr_lambda_debug<const char*, const decltype(ret3)&>("ret3 is ", ret3);
+    auto ret4 = c2(6);
+    ptr_lambda_debug<const char*, const decltype(ret4)&>("ret4 is ", ret4);
+}
+
 int main(void) {
     puts("START 項目 31 ：デフォルトのキャプチャモードは避ける ===");
+    if(0.01) {
+        ptr_lambda_debug<const char*, const int&>("Play and Result ... ", test_debug_and_error());
+    }
+    if(1.00) {
+        sample();
+    }
     puts("=== 項目 31 ：デフォルトのキャプチャモードは避ける END");
     return 0;
 }
