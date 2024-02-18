6fbf7df0e8cf9661f681ccfbe3edfb98bcdd2f00
Jack <alienm14xr2@gmail.com>
2024-02-18 22:18:17 +0900

    Effective Modern C++ - 6 章 ラムダ式。 - 項目 32 ：クロージャ内にオブジェクトをムーブする場面では初期化キャプチャを用いる。 - 新規ソースファイル作成。

---
+++ Effective-Modern-C++/src/chapter_6_lambda_32.cpp
@@ -0,0 +1,13 @@
+/**
+ * 6 章 ラムダ式
+ * 
+ * 項目 32 ：クロージャ内にオブジェクトをムーブする場面では初期化キャプチャを用いる
+ * 
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_6_lambda_32.cpp -o ../bin/main
+*/
+#include <iostream>
+
+int main(void) {
+    puts("START 項目 32 ：クロージャ内にオブジェクトをムーブする場面では初期化キャプチャを用いる ===");
+    puts("=== 項目 32 ：クロージャ内にオブジェクトをムーブする場面では初期化キャプチャを用いる END");
+}
