e24b09dfb4369682210cdaef73ff67adafbaf653
Jack <alienm14xr2@gmail.com>
2024-02-19 13:41:28 +0900

    Effective Modern C++ - 6 章 ラムダ式。 - 項目 33 ：auto&& 仮引数を std::forward する場合は decltype を用いる。 - 新規ソースファイル作成。

---
+++ Effective-Modern-C++/src/chapter_6_lambda_33.cpp
@@ -0,0 +1,15 @@
+/**
+ * 6 章 ラムダ式
+ * 
+ * 項目 33 ：auto&& 仮引数を std::forward する場合は decltype を用いる
+ * 
+ * e.g. compile.
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_6_lambda_33.cpp -o ../bin/main
+*/
+#include <iostream>
+
+int main(void) {
+    puts("START 項目 33 ：auto&& 仮引数を std::forward する場合は decltype を用いる ===");
+    puts("=== 項目 33 ：auto&& 仮引数を std::forward する場合は decltype を用いる END");
+    return 0;
+}
