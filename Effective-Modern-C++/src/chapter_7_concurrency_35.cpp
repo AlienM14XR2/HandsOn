67c6452aecca3913bf63e02c3c0aec0f7aa00952
Jack <alienm14xr2@gmail.com>
2024-02-19 17:56:16 +0900

    Effective Modern C++ - 7 章 並行 API。 - 項目 35 ：スレッドベースよりもタスクベースプログラミングを優先する。 - 新規ソースファイル作成。

---
+++ Effective-Modern-C++/src/chapter_7_concurrency_35.cpp
@@ -0,0 +1,15 @@
+/**
+ * 7 章 並行 API
+ * 
+ * 項目 35 ：スレッドベースよりもタスクベースプログラミングを優先する
+ * 
+ * e.g. compile.
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_7_concurrency_35.cpp -o ../bin/main
+*/
+#include <iostream>
+
+int main(void) {
+    puts("START 項目 35 ：スレッドベースよりもタスクベースプログラミングを優先する ===");
+    puts("=== 項目 35 ：スレッドベースよりもタスクベースプログラミングを優先する END");
+    return 0;
+}
