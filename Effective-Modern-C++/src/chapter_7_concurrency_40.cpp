98645967827e978b392fd55e25e2bf52d5c950dc
Jack <alienm14xr2@gmail.com>
2024-02-23 01:32:48 +0900

    Effective Modern C++ - 7 章 並行 API。 - 項目 40 ：並行処理には std::atomic を、特殊メモリには volatile を用いる。 - 新規ソースファイル作成。

---
+++ Effective-Modern-C++/src/chapter_7_concurrency_40.cpp
@@ -0,0 +1,58 @@
+/**
+ * 7 章 並行 API
+ * 
+ * 項目 40 ：並行処理には std::atomic を、特殊メモリには volatile を用いる
+ * 
+ * 可哀想なことに volatile はとても誤解されています。そもそも並行プログラミングに関係しないのですから。本章に含めるべきでもありません。
+ * しかし、他のプログラミング言語（Java や C# など）、volatile は並行プログラミングで重宝されるものです。
+ * プログラマが volatile ー 紛れもなく本項で解説する機能 ー とたびたび混同する C++ 機能が std::atomic テンプレートです。std::atomic 
+ * テンプレートをインスタンス化すると、他のスレッドから見てアトミックな動作を保証できます。
+ * 
+ * e.g. compile.
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror chapter_7_concurrency_40.cpp -o ../bin/main
+*/
+#include <iostream>
+#include <cassert>
+
+template <class M, class D>
+void (*ptr_lambda_debug)(M, D) = [](const auto message, const auto debug) -> void {
+    std::cout << "DEBUG: " << message << '\t' << debug << std::endl;
+};
+
+template <class Err>
+concept ErrReasonable = requires(Err& e) {
+    e.what();
+};
+
+template <class Err>
+requires ErrReasonable<Err>
+void (*ptr_print_error)(Err) = [](const auto e) {
+    std::cout << "ERROR: " << e.what() << std::endl;
+};
+
+int test_debug_and_error() {
+    puts("=== test_debug_and_error");
+    try {
+        // ptr_print_now();
+        auto pi = 3.141592;
+        ptr_lambda_debug<const char*,const decltype(pi)&>("pi is ", pi);
+        ptr_lambda_debug<const char*,const decltype(pi)*>("pi addr is ", &pi);
+        throw std::runtime_error("It's test error.");
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        printf("e type is ... %s\n", typeid(e).name());
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
+int main(void) {
+    puts("START 項目 40 ：並行処理には std::atomic を、特殊メモリには volatile を用いる ===");
+    if(0.01) {
+        auto ret = 0;
+        ptr_lambda_debug<const char*, const int&>("Play and Result ... ", ret = test_debug_and_error());
+        assert(ret == 1);
+    }
+    puts("===   項目 40 ：並行処理には std::atomic を、特殊メモリには volatile を用いる END");
+    return 0;
+}
