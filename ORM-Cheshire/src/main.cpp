103686a9873187b8fd41b8f60979cc07b726f616
Jack <alienm14xr2@gmail.com>
2024-02-25 16:11:48 +0900

    O/R Mapping - Lost Chapter。 - Coffee Break 版のグローバル関数の移植完了。 - そのテストの移植完了。

--- ORM-Cheshire/src/main.cpp
+++ ORM-Cheshire/src/main.cpp
@@ -126,6 +126,213 @@ int test_PersonData() {
     }
 }
 
+/**
+ * リポジトリ案 A
+*/
+
+template <class Data, class PKEY>
+class Repository {
+    bool insert(const Data&) = 0;
+    bool update(const Data&) = 0;
+    bool remove(const PKEY&) = 0;
+    Data findOne(const PKEY&) = 0;
+};
+
+/**
+ * SQL 文の構築に関する考察
+ * 
+ * INSERT INTO テーブル名 (列1, 列2, ...)
+ * VALUES (値1, 値2, ...);
+ *
+ * INSERT INTO customer (name, route, saved_date, price) 
+ * VALUES ('松田', 'ad2', '2023-05-24 19:49:28', 2500);
+ *
+ * prep_stmt = con->prepareStatement("INSERT INTO test(id, label) VALUES (?, ?)");      // MySQL の Prepared Statements の例
+ * 
+ * SQL インジェクションの可能性を考慮すれば、Prepared Statements の利用は必須と考える。
+ * よって、今回の 動的 SQL 文の最終型は 『"INSERT INTO test (id, label) VALUES (?, ?)"』とする。
+ * 
+ * カラム名とその数分の ? の出力ということ。
+ * 
+*/
+
+std::string makeInsertSql(const std::string& tableName, const std::vector<std::string>& colNames) {
+    std::string sql("INSERT INTO ");
+    sql.append(tableName);
+    // カラム
+    std::string cols(" (");
+    // 値
+    std::string vals("VALUES (");
+    for(std::size_t i=0 ; i<colNames.size(); i++) {
+        cols.append(colNames.at(i));
+        vals.append("?");
+        if( i < colNames.size()-1 ) {
+            cols.append(", ");
+            vals.append(", ");
+        } 
+    }
+    cols.append(") ");
+    vals.append(")");
+    sql.append(std::move(cols));
+    sql.append(std::move(vals));
+    return sql;
+}
+
+int test_makeInsertSql() {
+    puts("=== test_makeInsertSql");
+    try {
+        std::unique_ptr<RdbStrategy<PersonData>> strategy = std::make_unique<PersonStrategy>(PersonStrategy());
+        DataField<std::string> name("name", "Derek");
+        DataField<std::string> email("email", "derek@loki.org");
+        DataField<int> age("age", 21);
+        PersonData derek(std::move(strategy),name,email,age);
+
+        auto sql = makeInsertSql(derek.getTableName(), derek.getColumns());
+        ptr_lambda_debug<const char*,const decltype(sql)&>("sql: ", sql);
+
+        std::unique_ptr<RdbStrategy<PersonData>> strategy2 = std::make_unique<PersonStrategy>(PersonStrategy());
+        DataField<std::string> name2("name", "Cheshire");
+        std::optional<DataField<std::string>> empty_email;
+        std::optional<DataField<int>> empty_age;
+        PersonData cheshire(std::move(strategy2),name2,empty_email,empty_age);
+
+        auto sql2 = makeInsertSql(cheshire.getTableName(), cheshire.getColumns());
+        ptr_lambda_debug<const char*,const decltype(sql2)&>("sql2: ", sql2);
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
+/**
+ * UPDATE (表名) SET (カラム名1) = (値1) WHERE id = ?
+ * 
+ * UPDATE table_reference
+    SET col_name1 = value1 [, col_name2 = value2, ...]
+    [WHERE where_condition]
+ * 
+ * UPDATE test SET name = ?, label = ? WHERE id = ?
+ * 
+ * 更新すべき対象は 1行 としたい、Pkey を条件にする。
+*/
+
+std::string makeUpdateSql(const std::string& tableName, const std::string& pkName, const std::vector<std::string>& colNames ) {
+    std::string sql("UPDATE ");
+    sql.append(tableName);
+    std::string set(" SET ");
+    std::string condition(" WHERE ");
+    condition.append(pkName).append(" = ?");
+    for(std::size_t i=0; i<colNames.size(); i++) {
+        set.append(colNames.at(i)).append(" = ?");
+        if( i < colNames.size()-1 ) {
+            set.append(", ");
+        } 
+
+    }
+    sql.append(set).append(condition);
+    return sql;    
+}
+
+int test_makeUpdateSql() {
+    puts("=== test_makeUpdateSql");
+    try {
+        std::unique_ptr<RdbStrategy<PersonData>> strategy = std::make_unique<PersonStrategy>(PersonStrategy());
+        DataField<std::string> name("name", "Derek");
+        DataField<std::string> email("email", "derek@loki.org");
+        DataField<int> age("age", 21);
+        PersonData derek(std::move(strategy),name,email,age);
+        auto[pk_nam, pk_val] = derek.getId().bind();
+        auto sql = makeUpdateSql(derek.getTableName(),pk_nam, derek.getColumns());
+        ptr_lambda_debug<const char*,const decltype(sql)&>("sql: ", sql);
+
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
+/**
+ * DELETE FROM table_name WHERE id = ?
+ * 
+ * 削除すべき対象は 1行 としたい、Pkey を条件にする。
+*/
+
+std::string makeDeleteSql(const std::string& tableName, const std::string& pkName) {
+    std::string sql("DELETE FROM ");
+    sql.append(tableName).append(" WHERE ").append(pkName).append(" = ?");
+    return sql;
+}
+
+int test_makeDeleteSql() {
+    puts("=== test_makeDeleteSql");
+    try {
+        std::unique_ptr<RdbStrategy<PersonData>> strategy = std::make_unique<PersonStrategy>(PersonStrategy());
+        DataField<std::string> name("name", "Derek");
+        DataField<std::string> email("email", "derek@loki.org");
+        DataField<int> age("age", 21);
+        PersonData derek(std::move(strategy),name,email,age);
+        auto[pk_nam, pk_val] = derek.getId().bind();
+        auto sql = makeDeleteSql(derek.getTableName(),pk_nam);
+        ptr_lambda_debug<const char*,const decltype(sql)&>("sql: ", sql);
+
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
+/**
+ * SELECT col1, col2, col3 FROM table WHERE primary-key = ?
+ * SELECT primary-key, col1, col2, col3 FROM table WHERE primary-key = ?
+ * 
+ * 仕様の問題、現状は pkey は RDBMS の Auto Increment を利用することを想定している、これは、次の実装に影響が及ぶ。
+ * - PersonStrategy::getColumns メンバ関数に pkey を含めるか否かということ。含めた場合は、別途シーケンステーブルと pkey の取得処理が必要になる。
+ * - makeFindOne の SELECT 句 の pkey の取り扱い方法について、上記に依存するため、この関数も本質的には 2 つ必要と思われる。
+ * 
+ * colNames に pkey は存在しないものとして次の関数は実装する。
+ * 
+*/
+
+std::string makeFindOneSql(const std::string& tableName, const std::string& pkeyName, const std::vector<std::string>& colNames) {
+    std::string sql("SELECT ");
+    std::string cols(pkeyName);
+    cols.append(", ");
+    for(std::size_t i = 0; i < colNames.size(); i++) {
+        cols.append(colNames.at(i));
+        if( i < colNames.size()-1 ) {
+            cols.append(", ");
+        } 
+    }
+    sql.append(cols).append(" FROM ").append(tableName).append(" WHERE ").append(pkeyName).append(" = ?");
+    return sql;
+}
+
+int test_makeFindOneSql() {
+    puts("=== test_makeFindOneSql");
+    try {
+        std::unique_ptr<RdbStrategy<PersonData>> strategy = std::make_unique<PersonStrategy>(PersonStrategy());
+        DataField<std::string> name("name", "Derek");
+        DataField<std::string> email("email", "derek@loki.org");
+        DataField<int> age("age", 21);
+        PersonData derek(std::move(strategy),name,email,age);
+        auto[pk_nam, pk_val] = derek.getId().bind();
+        auto sql = makeFindOneSql(derek.getTableName(), pk_nam, derek.getColumns());
+        ptr_lambda_debug<const char*, const decltype(sql)&>("sql: ", sql);
+
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
+/**
+ * TODO 各 テーブル情報を管理するクラスに CREATE TABLE 文を自動作成する機能がほしい。
+*/
+
 int main(void) {
     puts("START Lost Chapter O/R Mapping ===");
     if(0.01) {
@@ -140,6 +347,17 @@ int main(void) {
         ptr_lambda_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_DataField_2());
         assert(ret == 0);
         ptr_lambda_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_PersonData());
+        assert(ret == 0);
+    }
+    if(1.01) {
+        auto ret = 0;
+        ptr_lambda_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_makeInsertSql());
+        assert(ret == 0);
+        ptr_lambda_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_makeUpdateSql());
+        assert(ret == 0);
+        ptr_lambda_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_makeDeleteSql());
+        assert(ret == 0);
+        ptr_lambda_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_makeFindOneSql());
         assert(ret == 0);
     }
     puts("===   Lost Chapter O/R Mapping END");
