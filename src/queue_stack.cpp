470e2c33dac4893902519bdfae5a4741fa796642
Jack <alienm14xr2@gmail.com>
2024-03-01 00:34:52 +0900

    O/R Mapping - Lost Chapter。 - O/R Mapping Step 0。 - std::queue の動作確認。

--- src/queue_stack.cpp
+++ src/queue_stack.cpp
@@ -8,6 +8,7 @@
 #include <cassert>
 #include <queue>
 #include <stack>
+#include <memory>
 
 template <class M, class D>
 void (*ptr_lambda_debug)(M, D) = [](const auto message, const auto debug) -> void {
@@ -40,12 +41,60 @@ int test_debug_and_error() {
     }
 }
 
+/**
+ * 本編はここから。
+*/
+
+class Widget {
+public:
+    Widget(const int& _value): value(_value) 
+    {}
+    int getValue() const { return value; }
+private:
+    int value;
+};
+
+int test_queue() {
+    puts("=== test_queue");
+    try {
+        std::unique_ptr<Widget> wp1 = std::make_unique<Widget>(Widget(3));
+        ptr_lambda_debug<const char*, Widget*>("wp1 addr is ", wp1.get());
+        std::unique_ptr<Widget> wp2 = std::make_unique<Widget>(Widget(6));
+        std::unique_ptr<Widget> wp3 = std::make_unique<Widget>(Widget(9));
+
+        std::queue<Widget*> q;
+        q.push(wp1.get());
+        q.push(wp2.get());
+        q.push(wp3.get());
+
+        while(!q.empty()) {
+            Widget* wp  = q.front();
+            q.pop();
+            ptr_lambda_debug<const char*, const int&>("value is ", wp->getValue());
+        }
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
+
+/**
+ * 何らかのオブジェクトを Pool する仕組みは、標準の std::queue や std::stack でよさそう。 
+*/
+
 int main(void) {
     puts("START std::queue と std::stack ===");
     if(0.01) {
         auto ret = 0;
         ptr_lambda_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_debug_and_error());
         assert(ret == 1);   // テスト関数内で明示的にエラーにしているため。
+    }
+    if(1.00) {
+        auto ret = 0;
+        ptr_lambda_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_queue());
+        assert(ret == 0);
     }
     puts("=== std::queue と std::stack   END");
     return 0;
