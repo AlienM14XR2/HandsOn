83dd6f78237660de7378ccf2e15fcdc327ff3a7f
Jack <alienm14xr2@gmail.com>
2024-03-01 04:24:18 +0900

    O/R Mapping - Lost Chapter。 - O/R Mapping Step 0。 - トランザクションとリポジトリの実験用ソースを新規作成した。

---
+++ src/transaction_repository.cpp
@@ -0,0 +1,13 @@
+/**
+ * トランザクションとリポジトリを如何に抽象化できるか。
+ * 
+ * e.g. compile.
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror transaction_repository.cpp -o ../bin/main
+*/
+#include <iostream>
+
+int main(void) {
+    puts("START トランザクションとリポジトリを如何に抽象化できるか ===");
+    puts("===   トランザクションとリポジトリを如何に抽象化できるか END");
+    return 0;
+}
