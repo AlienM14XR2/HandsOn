e01c05ba97d8083343b0b1eb4fafef1de4b34c12
Jack <alienm14xr2@gmail.com>
2024-03-01 04:29:17 +0900

    O/R Mapping - Lost Chapter。 - O/R Mapping Step 0。 - トランザクションとリポジトリの実験用ソースを新規作成した（テンプレ確認）。

--- src/transaction_repository.cpp
+++ src/transaction_repository.cpp
@@ -5,9 +5,46 @@
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror transaction_repository.cpp -o ../bin/main
 */
 #include <iostream>
+#include <cassert>
+
+template <class M, class D>
+void (*ptr_lambda_debug)(M, D) = [](const auto message, const auto debug) -> void {
+    std::cout << "DEBUG: " << message << '\t' << debug << std::endl;
+};
+
+template <class Error>
+concept ErrReasonable = requires(Error& e) {
+    e.what();
+};
+template <class Error>
+requires ErrReasonable<Error>
+void (*ptr_print_error)(Error) = [](const auto e) -> void {
+    std::cout << "ERROR: " << e.what() << std::endl;
+};
+
+int test_debug_and_error() {
+    puts("=== test_debug_and_error");
+    try {
+        // ptr_print_now();
+        auto pi = 3.141592;
+        ptr_lambda_debug<const char*,const decltype(pi)&>("pi is ", pi);
+        ptr_lambda_debug<const char*,const decltype(pi)*>("pi addr is ", &pi);
+        throw std::runtime_error("It's test error.");
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        printf("e type is ... %s\n", typeid(e).name());
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
 
 int main(void) {
     puts("START トランザクションとリポジトリを如何に抽象化できるか ===");
+    if(0.01) {
+        auto ret = 0;
+        ptr_lambda_debug<const char*,const int&>("Play and Result ... ", ret = test_debug_and_error());
+        assert(ret == 1);
+    }
     puts("===   トランザクションとリポジトリを如何に抽象化できるか END");
     return 0;
 }
