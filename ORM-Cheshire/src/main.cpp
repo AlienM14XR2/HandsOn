2954866933e9345814bf2615f1d9796a26512088
Jack <alienm14xr2@gmail.com>
2024-03-02 10:18:42 +0900

    O/R Mapping - Lost Chapter。 - O/R Mapping 。 - 既存テスト関数を main から分離して test_1.cpp、test_1.hpp に移動した。

--- ORM-Cheshire/src/main.cpp
+++ ORM-Cheshire/src/main.cpp
@@ -45,7 +45,7 @@
  * ```
  * 
  * e.g. compile A.
- * g++ -O3 -DDEBUG -std=c++20 -I../inc/ -I/usr/include/mysql-cppconn-8/ -L/usr/lib/x86_64-linux-gnu/ -pedantic-errors -Wall -Werror main.cpp -lmysqlcppconn -lmysqlcppconn8 ./model/PersonStrategy.cpp ./data/PersonData.cpp ./driver/MySQLDriver.cpp -o ../bin/main
+ * g++ -O3 -DDEBUG -std=c++20 -I../inc/ -I/usr/include/mysql-cppconn-8/ -L/usr/lib/x86_64-linux-gnu/ -pedantic-errors -Wall -Werror main.cpp -lmysqlcppconn -lmysqlcppconn8 ./model/PersonStrategy.cpp ./data/PersonData.cpp ./driver/MySQLDriver.cpp ./test/test_1.cpp -o ../bin/main
  * 
  * e.g. compile B. 
  * 分割した方が少しだけコンパイル時間が短縮できるかな、体感値で申し訳ないが。
@@ -55,7 +55,8 @@
  * g++ -O3 -DDEBUG -std=c++20 -I../inc/ -pedantic-errors -Wall -Werror -c ./data/PersonData.cpp -o ../bin/PersonData.o
  * g++ -O3 -DDEBUG -std=c++20 -I../inc/ -pedantic-errors -Wall -Werror -c ./model/PersonStrategy.cpp -o ../bin/PersonStrategy.o
  * g++ -O3 -DDEBUG -std=c++20 -I../inc/ -pedantic-errors -Wall -Werror -c ./driver/MySQLDriver.cpp -o ../bin/MySQLDriver.o
- * g++ -O3 -DDEBUG -std=c++20 -I../inc/ -I/usr/include/mysql-cppconn-8/ -L/usr/lib/x86_64-linux-gnu/ -pedantic-errors -Wall -Werror main.cpp -lmysqlcppconn -lmysqlcppconn8 ../bin/PersonStrategy.o ../bin/PersonData.o ../bin/MySQLDriver.o -o ../bin/main
+ * g++ -O3 -DDEBUG -std=c++20 -I../inc/ -pedantic-errors -Wall -Werror -c ./test/test_1.cpp -o ../bin/test_1.o
+ * g++ -O3 -DDEBUG -std=c++20 -I../inc/ -I/usr/include/mysql-cppconn-8/ -L/usr/lib/x86_64-linux-gnu/ -pedantic-errors -Wall -Werror main.cpp -lmysqlcppconn -lmysqlcppconn8 ../bin/PersonStrategy.o ../bin/PersonData.o ../bin/MySQLDriver.o ../bin/test_1.o -o ../bin/main
 */
 
 #include <iostream>
@@ -73,6 +74,7 @@
 #include "../inc/PersonData.hpp"
 #include "../inc/MySQLDriver.hpp"
 #include "../inc/ConnectionPool.hpp"
+#include "../inc/test_1.hpp"
 #include "/usr/include/mysql-cppconn-8/mysql/jdbc.h"
 #include "/usr/include/mysql-cppconn-8/mysqlx/xdevapi.h"
 
@@ -92,30 +94,6 @@ int test_debug_and_error() {
     }
 }
 
-int test_DataField() {
-    puts("=== test_DataField");
-    try {
-        DataField<int> d1("id",1);
-        auto[name, value] = d1.bind();
-        ptr_lambda_debug<const char*,const decltype(value)&>("value is ", value);
-        ptr_lambda_debug<const char*,const std::string&>("value type is ", typeid(value).name());   // Boost のライブラリの方が正確との情報があった。
-        ptr_lambda_debug<const char*,const decltype(name)&>("name is ", name);
-
-        DataField<std::string> d2("name", "Alice");
-        auto[name2, value2] = d2.bind();
-        ptr_lambda_debug<const char*,const decltype(value2)&>("value2 is ", value2);
-        ptr_lambda_debug<const char*,const std::string&>("value2 type is ", typeid(value2).name());   // Boost のライブラリの方が正確との情報があった。
-        ptr_lambda_debug<const char*,const decltype(name2)&>("name2 is ", name2);
-        /**
-         * TODO Jack
-         * 時間を見て、MySQL にある型と一致する C++ の型を網羅させる。
-        */
-        return EXIT_SUCCESS;
-    } catch(std::exception& e) {
-        ptr_print_error<const decltype(e)&>(e);
-        return EXIT_FAILURE;
-    }
-}
 
 class Widget {
 public:
@@ -126,93 +104,19 @@ private:
     int value;
 };
 
-int test_DataField_2() {
-    puts("=== test_DataField_2");
-    try {
-        // タプルの動作確認
-        DataField<int> d1("id", 3, "integer");
-        auto[name, value, type] = d1.bindTuple();
-        ptr_lambda_debug<const char*, const decltype(name)&>("name is ", name);
-        ptr_lambda_debug<const char*, const decltype(value)&>("value is ", value);
-        ptr_lambda_debug<const char*, const decltype(type)&>("sql type is ", type);
-        return EXIT_SUCCESS;
-    } catch(std::exception& e) {
-        ptr_print_error<const decltype(e)&>(e);
-        return EXIT_FAILURE;
-    }
-}
 
-int test_DataField_3() {
-    puts("=== test_DataField_3");
-    try {
-        DataField<int> d1("id", 3, "integer", "PRIMARY KEY");
-        auto[name, type, constraint] = d1.bindTupleTblInfo();
-        ptr_lambda_debug<const char*, const decltype(name)&>("name is ", name);
-        ptr_lambda_debug<const char*, const decltype(type)&>("sql type is ", type);
-        ptr_lambda_debug<const char*, const decltype(constraint)&>("constraint is ", constraint);
-        // テスト内で次をやらないと厳密なテストとは言えない。
-        assert( name == "id" );
-        assert( type == "integer" );
-        assert( constraint == "PRIMARY KEY" );
-        return EXIT_SUCCESS;
-    } catch(std::exception& e) {
-        ptr_print_error<const decltype(e)&>(e);
-        return EXIT_FAILURE;
-    }
-}
-
-int test_PersonData() {
-    puts("=== test_PersonData");
-    try {
-        std::unique_ptr<RdbStrategy<PersonData>> strategy = std::make_unique<PersonStrategy>(PersonStrategy());
-        DataField<std::string> name("name", "Derek");
-        DataField<std::string> email("email", "derek@loki.org");
-        DataField<int> age("age", 21);
-        PersonData derek(std::move(strategy),name,email,age);
-
-        ptr_lambda_debug<const char*,const std::string&>("table is ", derek.getTableName());
-        auto[nam, val] = derek.getName().bind();
-        ptr_lambda_debug<const char*, const decltype(nam)&>("name is ", nam);        
-        ptr_lambda_debug<const char*, const decltype(val)&>("value is ", val);        
-        auto[nam2, val2] = derek.getEmail().bind();
-        ptr_lambda_debug<const char*, const decltype(nam2)&>("name is ", nam2);        
-        ptr_lambda_debug<const char*, const decltype(val2)&>("value is ", val2);
-        auto[nam3, val3] = derek.getAge().value().bind();
-        ptr_lambda_debug<const char*, const decltype(nam3)&>("name is ", nam3);        
-        ptr_lambda_debug<const char*, const decltype(val3)&>("value is ", val3);
-
-        /**
-         * 次はこのテーブル情報を持ったクラスから、動的に SQL 生成ができればよい、具体的には以下。
-         * - INSERT 文
-         * - UPDATE 文
-         * - DELETE 文
-         * - SELECT 文 これは、findOne() のみでよい。
-         * これらは定型文なので自動で SQL を生成する仕組みがあった方が絶対にいい。
-         * 
-        */
-        auto cols = derek.getColumns();
-        for(auto name: cols) {
-            ptr_lambda_debug<const char*, const decltype(name)&>("name is ", name);
-        }
-        assert(cols.size() == 3U);
-        return EXIT_SUCCESS;
-    } catch(std::exception& e) {
-        ptr_print_error<const decltype(e)&>(e);
-        return EXIT_FAILURE;
-    }
-}
 
 /**
  * リポジトリ案 A
 */
 
-template <class Data, class PKEY>
-class Repository {
-    bool insert(const Data&) = 0;
-    bool update(const Data&) = 0;
-    bool remove(const PKEY&) = 0;
-    Data findOne(const PKEY&) = 0;
-};
+// template <class Data, class PKEY>
+// class Repository {
+//     bool insert(const Data&) = 0;
+//     bool update(const Data&) = 0;
+//     bool remove(const PKEY&) = 0;
+//     Data findOne(const PKEY&) = 0;
+// };
 
 /**
  * SQL 文の構築に関する考察
@@ -254,32 +158,6 @@ std::string makeInsertSql(const std::string& tableName, const std::vector<std::s
     return sql;
 }
 
-int test_makeInsertSql() {
-    puts("=== test_makeInsertSql");
-    try {
-        std::unique_ptr<RdbStrategy<PersonData>> strategy = std::make_unique<PersonStrategy>(PersonStrategy());
-        DataField<std::string> name("name", "Derek");
-        DataField<std::string> email("email", "derek@loki.org");
-        DataField<int> age("age", 21);
-        PersonData derek(std::move(strategy),name,email,age);
-
-        auto sql = makeInsertSql(derek.getTableName(), derek.getColumns());
-        ptr_lambda_debug<const char*,const decltype(sql)&>("sql: ", sql);
-
-        std::unique_ptr<RdbStrategy<PersonData>> strategy2 = std::make_unique<PersonStrategy>(PersonStrategy());
-        DataField<std::string> name2("name", "Cheshire");
-        DataField<std::string> email2("email", "cheshire@loki.org");
-        std::optional<DataField<int>> empty_age;
-        PersonData cheshire(std::move(strategy2),name2,email2,empty_age);
-
-        auto sql2 = makeInsertSql(cheshire.getTableName(), cheshire.getColumns());
-        ptr_lambda_debug<const char*,const decltype(sql2)&>("sql2: ", sql2);
-        return EXIT_SUCCESS;
-    } catch(std::exception& e) {
-        ptr_print_error<const decltype(e)&>(e);
-        return EXIT_FAILURE;
-    }
-}
 
 /**
  * UPDATE (表名) SET (カラム名1) = (値1) WHERE id = ?
@@ -310,24 +188,6 @@ std::string makeUpdateSql(const std::string& tableName, const std::string& pkNam
     return sql;    
 }
 
-int test_makeUpdateSql() {
-    puts("=== test_makeUpdateSql");
-    try {
-        std::unique_ptr<RdbStrategy<PersonData>> strategy = std::make_unique<PersonStrategy>(PersonStrategy());
-        DataField<std::string> name("name", "Derek");
-        DataField<std::string> email("email", "derek@loki.org");
-        DataField<int> age("age", 21);
-        PersonData derek(std::move(strategy),name,email,age);
-        auto[pk_nam, pk_val] = derek.getId().bind();
-        auto sql = makeUpdateSql(derek.getTableName(),pk_nam, derek.getColumns());
-        ptr_lambda_debug<const char*,const decltype(sql)&>("sql: ", sql);
-
-        return EXIT_SUCCESS;
-    } catch(std::exception& e) {
-        ptr_print_error<const decltype(e)&>(e);
-        return EXIT_FAILURE;
-    }
-}
 
 /**
  * DELETE FROM table_name WHERE id = ?
@@ -341,24 +201,6 @@ std::string makeDeleteSql(const std::string& tableName, const std::string& pkNam
     return sql;
 }
 
-int test_makeDeleteSql() {
-    puts("=== test_makeDeleteSql");
-    try {
-        std::unique_ptr<RdbStrategy<PersonData>> strategy = std::make_unique<PersonStrategy>(PersonStrategy());
-        DataField<std::string> name("name", "Derek");
-        DataField<std::string> email("email", "derek@loki.org");
-        DataField<int> age("age", 21);
-        PersonData derek(std::move(strategy),name,email,age);
-        auto[pk_nam, pk_val] = derek.getId().bind();
-        auto sql = makeDeleteSql(derek.getTableName(),pk_nam);
-        ptr_lambda_debug<const char*,const decltype(sql)&>("sql: ", sql);
-
-        return EXIT_SUCCESS;
-    } catch(std::exception& e) {
-        ptr_print_error<const decltype(e)&>(e);
-        return EXIT_FAILURE;
-    }
-}
 
 /**
  * SELECT col1, col2, col3 FROM table WHERE primary-key = ?
@@ -386,24 +228,6 @@ std::string makeFindOneSql(const std::string& tableName, const std::string& pkey
     return sql;
 }
 
-int test_makeFindOneSql() {
-    puts("=== test_makeFindOneSql");
-    try {
-        std::unique_ptr<RdbStrategy<PersonData>> strategy = std::make_unique<PersonStrategy>(PersonStrategy());
-        DataField<std::string> name("name", "Derek");
-        DataField<std::string> email("email", "derek@loki.org");
-        DataField<int> age("age", 21);
-        PersonData derek(std::move(strategy),name,email,age);
-        auto[pk_nam, pk_val] = derek.getId().bind();
-        auto sql = makeFindOneSql(derek.getTableName(), pk_nam, derek.getColumns());
-        ptr_lambda_debug<const char*, const decltype(sql)&>("sql: ", sql);
-
-        return EXIT_SUCCESS;
-    } catch(std::exception& e) {
-        ptr_print_error<const decltype(e)&>(e);
-        return EXIT_FAILURE;
-    }
-}
 
 /**
  * TODO 各 テーブル情報を管理するクラスに CREATE TABLE 文を自動作成する機能がほしい。
@@ -446,29 +270,6 @@ std::string makeCreateTableSql(const std::string& tableName, const std::vector<s
     return sql;
 }
 
-int test_makeCreateTableSql() {
-    puts("=== test_makeCreateTableSql");
-    try {
-        std::unique_ptr<RdbStrategy<PersonData>> strategy = std::make_unique<PersonStrategy>(PersonStrategy());
-        DataField<std::size_t> id("id", 0, "BIGINT", "AUTO_INCREMENT PRIMARY KEY");  // MySQL でこの構文で問題がないか要検証。
-        DataField<std::string> name("name", "Derek", "VARCHAR(128)", "NOT NULL");
-        DataField<std::string> email("email", "derek@loki.org", "VARCHAR(256)", "NOT NULL UNIQUE");
-        DataField<int> age("age", 21, "INT", "");
-        PersonData derek(std::move(strategy),id,name,email,age);
-        auto tblInfos = derek.getTableInfo();
-        for(auto info: tblInfos) {
-            ptr_lambda_debug<const char*, const decltype(get<0>(info))&>("name is ", get<0>(info));
-            ptr_lambda_debug<const char*, const decltype(get<1>(info))&>("type is ", get<1>(info));
-            ptr_lambda_debug<const char*, const decltype(get<2>(info))&>("const is ", get<2>(info));
-        }
-        auto sql = makeCreateTableSql(derek.getTableName(),derek.getTableInfo());
-        ptr_lambda_debug<const char*, const decltype(sql)&>("sql is ", sql);
-        return EXIT_SUCCESS;
-    } catch(std::exception& e) {
-        ptr_print_error<const decltype(e)&>(e);
-        return EXIT_FAILURE;
-    }
-}
 
 int test_mysql_connect() {
     puts("=== test_mysql_connect");
@@ -600,34 +401,6 @@ int test_insert_person() {
     }
 }
 
-int test_MySQLDriver() {
-    puts("=== test_MySQLDriver");
-    try {
-        MySQLDriver& md1 = MySQLDriver::getInstance();
-        MySQLDriver& md2 = MySQLDriver::getInstance();
-        ptr_lambda_debug<const char*, MySQLDriver*>("md1 addr is ", &md1);
-        ptr_lambda_debug<const char*, MySQLDriver*>("md2 addr is ", &md2);
-        assert(&md1 == &md2);
-
-        sql::Driver* d1 = md1.getDriver();
-        sql::Driver* d2 = md2.getDriver();
-        ptr_lambda_debug<const char*, sql::Driver*>("d1 addr is ", d1);
-        ptr_lambda_debug<const char*, sql::Driver*>("d2 addr is ", d2);
-        assert(d1 == d2);
-
-        RdbDriver<sql::Driver>* rd = &(MySQLDriver::getInstance());
-        ptr_lambda_debug<const char*, RdbDriver<sql::Driver>*>("rd addr is ", rd);
-        assert(rd == &md1);
-        sql::Driver* d3 = rd->getDriver();
-        ptr_lambda_debug<const char*, sql::Driver*>("d3 addr is ", d3);
-        assert(d3 == d1);
-
-        return EXIT_SUCCESS;
-    } catch(std::exception& e) {
-        ptr_print_error<const decltype(e)&>(e);
-        return EXIT_FAILURE;
-    }
-}
 
 /**
  * 次は Connection 管理の仕組みを考えてみる。
