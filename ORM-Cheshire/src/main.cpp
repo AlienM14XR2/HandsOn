f44bba024cbb2bf7803cb1ee4264caa45a9b2061
Jack <alienm14xr2@gmail.com>
2024-03-02 17:42:02 +0900

    O/R Mapping - Lost Chapter。 - O/R Mapping。 - RdbProcStrategy 実装、未テスト。

--- ORM-Cheshire/src/main.cpp
+++ ORM-Cheshire/src/main.cpp
@@ -661,6 +661,20 @@ public:
     virtual std::optional<DATA> proc() const = 0;
 };
 
+/**
+ * RdbProcStrategy クラス
+ * 
+ * RdbTransaction::proc のパリエーション・ポイントを表現するインタフェース。
+ * CRUD の個別の処理は、本クラスの派生クラスで実現すること。
+*/
+
+template <class DATA>
+class RdbProcStrategy {
+public:
+    virtual ~RdbProcStrategy() = default;
+    virtual std::optional<DATA> proc() const = 0;
+};
+
 
 /**
  * MySQL Shell
