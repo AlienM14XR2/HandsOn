1e62943e00ed31936a1b3175836e820768695033
Jack <alienm14xr2@gmail.com>
2024-03-03 14:01:35 +0900

    O/R Mapping - Lost Chapter。 - O/R Mapping。 - RdbProcStrategy.hpp ファイル作成（main から分離した）。

---
+++ ORM-Cheshire/inc/RdbProcStrategy.hpp
@@ -0,0 +1,20 @@
+#ifndef _RDBPROCSTRATEGY_H_
+#define _RDBPROCSTRATEGY_H_
+
+#include <optional>
+
+/**
+ * RdbProcStrategy クラス
+ * 
+ * RdbTransaction::proc のパリエーション・ポイントを表現するインタフェース。
+ * CRUD の個別の処理は、本クラスの派生クラスで実現すること。
+*/
+
+template <class DATA>
+class RdbProcStrategy {
+public:
+    virtual ~RdbProcStrategy() = default;
+    virtual std::optional<DATA> proc() const = 0;
+};
+
+#endif
--- ORM-Cheshire/src/main.cpp
+++ ORM-Cheshire/src/main.cpp
@@ -79,6 +79,7 @@
 #include "../inc/MySQLConnection.hpp"
 #include "../inc/Repository.hpp"
 #include "../inc/RdbTransaction.hpp"
+#include "../inc/RdbProcStrategy.hpp"
 #include "/usr/include/mysql-cppconn-8/mysql/jdbc.h"
 #include "/usr/include/mysql-cppconn-8/mysqlx/xdevapi.h"
 
@@ -556,19 +557,6 @@ std::optional<PersonData> PersonRepository::findOne(const std::size_t& pkey) con
 */
 
 
-/**
- * RdbProcStrategy クラス
- * 
- * RdbTransaction::proc のパリエーション・ポイントを表現するインタフェース。
- * CRUD の個別の処理は、本クラスの派生クラスで実現すること。
-*/
-
-template <class DATA>
-class RdbProcStrategy {
-public:
-    virtual ~RdbProcStrategy() = default;
-    virtual std::optional<DATA> proc() const = 0;
-};
 
 /**
  * MySQLCreateStrategy クラス
