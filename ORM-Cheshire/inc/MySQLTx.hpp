f9506239e8bae9ebcea960087fb48b8932b73d53
Jack <alienm14xr2@gmail.com>
2024-03-03 14:27:29 +0900

    O/R Mapping - Lost Chapter。 - O/R Mapping。 - MySQLTx.hpp ファイル作成（main から分離した）。

---
+++ ORM-Cheshire/inc/MySQLTx.hpp
@@ -0,0 +1,38 @@
+#ifndef _MYSQLTX_H_
+#define _MYSQLTX_H_
+
+#include <optional>
+#include "/usr/include/mysql-cppconn-8/mysql/jdbc.h"
+#include "RdbTransaction.hpp"
+
+/**
+ * MySQLTx クラス
+ * 
+ * RdbTransaction の派生クラス、MySQL の Tx を担う。 
+*/
+
+template <class DATA>
+class MySQLTx final : public RdbTransaction<DATA> {
+public:
+    MySQLTx(RdbConnection<sql::PreparedStatement>* _con, const RdbProcStrategy<DATA>* _strategy): con(_con), strategy(_strategy)
+    {}
+    // ...
+    virtual void begin()    const override {
+        con->begin();
+    }
+    virtual void commit()   const override {
+        con->commit();
+    }
+    virtual void rollback() const override {
+        con->rollback();
+    }
+    virtual std::optional<DATA> proc() const override {
+        return strategy->proc();
+    }
+
+private:
+    const RdbConnection<sql::PreparedStatement>* con;
+    const RdbProcStrategy<DATA>* strategy;
+};
+
+#endif
--- ORM-Cheshire/src/main.cpp
+++ ORM-Cheshire/src/main.cpp
@@ -81,6 +81,7 @@
 #include "../inc/RdbTransaction.hpp"
 #include "../inc/RdbProcStrategy.hpp"
 #include "../inc/MySQLCreateStrategy.hpp"
+#include "../inc/MySQLTx.hpp"
 #include "/usr/include/mysql-cppconn-8/mysql/jdbc.h"
 #include "/usr/include/mysql-cppconn-8/mysqlx/xdevapi.h"
 
@@ -560,35 +561,6 @@ std::optional<PersonData> PersonRepository::findOne(const std::size_t& pkey) con
 
 
 
-/**
- * MySQLTx クラス
- * 
- * RdbTransaction の派生クラス、MySQL の Tx を担う。 
-*/
-
-template <class DATA>
-class MySQLTx final : public RdbTransaction<DATA> {
-public:
-    MySQLTx(RdbConnection<sql::PreparedStatement>* _con, const RdbProcStrategy<DATA>* _strategy): con(_con), strategy(_strategy)
-    {}
-    // ...
-    virtual void begin()    const override {
-        con->begin();
-    }
-    virtual void commit()   const override {
-        con->commit();
-    }
-    virtual void rollback() const override {
-        con->rollback();
-    }
-    virtual std::optional<DATA> proc() const override {
-        return strategy->proc();
-    }
-
-private:
-    const RdbConnection<sql::PreparedStatement>* con;
-    const RdbProcStrategy<DATA>* strategy;
-};
 
 
 int test_MySQLTx() {
