b9a1f0c1a906c7f76c68dc2089407f0c056eb105
Jack <alienm14xr2@gmail.com>
2024-03-06 19:28:23 +0900

    O/R Mapping - Lost Chapter。 - O/R Mapping。 - 備忘録、コメント追記。

--- ORM-Cheshire/inc/MySQLCreateStrategy.hpp
+++ ORM-Cheshire/inc/MySQLCreateStrategy.hpp
@@ -7,6 +7,11 @@
  * MySQLCreateStrategy クラス
  * 
  * Create（Insert） を行う。
+ * 
+ * 補足、これは別に Repository と 1 対 1 である必要はなく proc（同一トランザクション） で必要なリポジトリを
+ * 派生クラスでは複数インジェクションできることを覚えておいてほしい（未来の私：）。
+ * これは、あくまでも最低限のサンプルであり、実際は複数のテーブルへの Insert、Update などが行われることが常
+ * である。
 */
 
 template <class DATA, class PKEY>
