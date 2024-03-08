64c056f091c7f7e69318fa4fc84f018bf42a302a
Jack <alienm14xr2@gmail.com>
2024-03-09 03:47:23 +0900

    課題 - FastCGI。 - Nginx 、C++ で fcgi を実行してみる。 - REST API の考察と実装・テスト（動確）。

--- src/fcgi_hello_world.cpp
+++ src/fcgi_hello_world.cpp
@@ -61,12 +61,93 @@ sudo tail -f /var/log/nginx/error.log
  * e.g. compile.
  * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror fcgi_hello_world.cpp -lfcgi++ -lfcgi -o ../bin/hello_world
 */
+#include <iostream>
+#include <memory>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcgi_config.h>
 #include <fcgi_stdio.h>
+
+template <class M, class D>
+void (*ptr_lambda_debug)(M, D) = [](const auto message, const auto debug) -> void {
+    std::cout << "DEBUG: " << message << "\t" << debug << std::endl;
+};
+
+template <class T>
+class Controller {
+public:
+    virtual ~Controller() = default;
+    virtual T execute() const = 0;
+};
+
+class CreatePersonCtr final : public Controller<std::string> {
+public:
+    static Controller* factory(const std::string& uri) {
+        printf("\nDEBUG: uri is %s\n", uri.c_str());
+        if(uri == "/hello_world/create/person/" || uri == "/hello_world/create/person") {
+            return new CreatePersonCtr();
+        }
+        return nullptr;
+    }
+    virtual std::string execute() const override {
+        puts("------ CreatePersonCtr::execute()");
+        return "CreatePersonCtr";
+    }
+};
+
+class ReadPersonCtr final : public Controller<std::string> {
+public:
+    static Controller* factory(const std::string& uri) {
+        if(uri == "/hello_world/read/person/" || uri == "/hello_world/read/person") {
+            return new ReadPersonCtr();
+        }
+        return nullptr;
+    }
+    virtual std::string execute() const override {
+        puts("------ ReadPersonCtr::execute()");
+        return "ReadPersonCtr";
+    }
+};
+
+class UpdatePersonCtr final : public Controller<std::string> {
+public:
+    static Controller* factory(const std::string& uri) {
+        if(uri == "/hello_world/update/person/" || uri == "/hello_world/update/person") {
+            return new UpdatePersonCtr();
+        }
+        return nullptr;
+    }
+    virtual std::string execute() const override {
+        puts("------ UpdatePersonCtr::execute()");
+        return "UpdatePersonCtr";
+    }
+};
+
+class DeletePersonCtr final : public Controller<std::string> {
+public:
+    static Controller* factory(const std::string& uri) {
+        if(uri == "/hello_world/delete/person/" || uri == "/hello_world/delete/person") {
+            return new DeletePersonCtr();
+        }
+        return nullptr;
+    }
+    virtual std::string execute() const override {
+        puts("------ DeletePersonCtr::execute()");
+        return "DeletePersonCtr";
+    }
+};
+
+void action(const Controller<std::string>* ctr) 
+{
+    std::string ret = ctr->execute();
+    printf("\nret is %s\n", ret.c_str());
+    ptr_lambda_debug<const char*, const std::string&>("ret is ", ret);  // これは標準出力としてコンソールに出力される
+    delete ctr;
+    ctr = nullptr;
+}
 
 int main()
 {
@@ -80,6 +161,28 @@ int main()
 
         printf("request uri is %s\n", getenv("REQUEST_URI"));   // これをもとに各処理に分岐できる、REST API のエンドポイントとして充分使えそう。
         printf("Request number %d running on host<i>%s</i>\n",++count,getenv("SERVER_NAME"));
+
+        Controller<std::string>* ctr = nullptr;
+        ctr = CreatePersonCtr::factory(getenv("REQUEST_URI"));
+        if(ctr) {   // このブロック内は関数化できるよね（DRY：）。
+            action(ctr);
+        }
+        ctr = ReadPersonCtr::factory(getenv("REQUEST_URI"));
+        if(ctr) {
+            action(ctr);
+        }
+        ctr = UpdatePersonCtr::factory(getenv("REQUEST_URI"));
+        if(ctr) {
+            action(ctr);
+        }
+        ctr = DeletePersonCtr::factory(getenv("REQUEST_URI"));
+        if(ctr) {
+            action(ctr);
+        }
+        /**
+        * URI の重複、コーディング・ミスがなければ、上記の実装でいいと思う。
+        */
+        
     }
     // while を抜けた際に、取得したメモリは解放する
     return 0;
@@ -91,7 +194,6 @@ int main()
  * 単純に考えれば
  * 
  * if(uir == "/api/insert/person/") {
- * 
  * } 
  * else if(...) {
  * }
