d1432dc7e86ae6de1781011c4d655c9d29b980bb
Jack <alienm14xr2@gmail.com>
2024-03-15 17:32:31 +0900

    Lost Chapter - ORM-Cheshire。 - mysqlx の利用。 - MySQLXCreateStrategy 実装、未テスト。

--- ORM-Cheshire/src/main.cpp
+++ ORM-Cheshire/src/main.cpp
@@ -270,6 +270,25 @@ private:
     const RdbProcStrategy<DATA>* strategy;
 };
 
+template<class DATA, class PKEY>
+class MySQLXCreateStrategy final : public RdbProcStrategy<DATA> {
+public:
+    MySQLXCreateStrategy(const Repository<DATA, PKEY>* _repo, const DATA& _data): repo(_repo), data(_data)
+    {}
+    // ...
+    virtual std::optional<DATA> proc() const override {
+        puts("------ MySQLXCreateStrategy::proc()");
+        try {
+            return repo->insert(data);
+        } catch(std::exception& e) {
+            throw std::runtime_error(e.what());
+        }
+    }
+private:
+    const Repository<DATA, PKEY>* repo;
+    DATA data;
+};
+
 }   // namespace ormx
 
 
