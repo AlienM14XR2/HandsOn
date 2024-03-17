721202a4697d8c8d887bbbff6e86cd2f65b77bb6
Jack <alienm14xr2@gmail.com>
2024-03-18 06:19:57 +0900

    課題 - Proxy パターンについて。 - CRUD 処理の動的作成。 - 考察とサンプリング。

--- 2024/src/proxy.cpp
+++ 2024/src/proxy.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <cassert>
 #include <chrono>
+#include <optional>
 #include <map>
 #include <vector>
 
@@ -57,16 +58,117 @@ int test_debug_error()
  * Repository をインタフェースとして、ProxyRepo と SubjectRepo がある。
 */
 
+class ORMData {
+public:
+    virtual ~ORMData() = default;
+    virtual std::string getTableName() const = 0;
+    virtual std::map<std::string, std::string> toMap() const = 0;
+};
+
+class PersonData final : public ORMData {
+public:
+    PersonData(const std::size_t& _id
+            , const std::string& _name
+            , const std::string& _email
+            , const int& _age ): id(_id), name(_name), email(_email), age(_age)
+    {}
+    PersonData(const std::string& _name
+            , const std::string& _email
+            , const int& _age ): id(0ul), name(_name), email(_email), age(_age)
+    {}
+    PersonData(const std::string& _name
+            , const std::string& _email ): id(0ul), name(_name), email(_email), age(std::nullopt)
+    {}
+    // ...
+    virtual std::string getTableName() const override
+    {
+        return "person";
+    }
+    virtual std::map<std::string, std::string> toMap() const override
+    {
+        // TODO 次の点を踏まえて、PKEY の std::optional を考慮してくれ。
+        // INSERT 文 で Auto-Increment を採用している場合は、PKEY は Key Value には不要なんだよな。
+        std::map<std::string, std::string> m{
+            {"id", std::to_string(id)}
+            , {"name", name}
+            , {"email", email}
+        };
+        if(age.has_value()) {
+            m.insert(std::make_pair("age", std::to_string(age.value())));
+        }
+        return m;
+    }
+
+    std::size_t getId()         const { return id; }
+    std::string getName()       const { return name; }
+    std::string getEmail()      const { return email; }
+    std::optional<int> getAge() const { return age; }
+private:
+    // TODO PKEY に該当するデータも std::optional の方が扱い易くないか、考えてみる。
+    std::size_t        id;
+    std::string        name;
+    std::string        email;
+    std::optional<int> age;
+};
+
 template <class DATA, class PKEY>
 class Repository {
 public:
     virtual ~Repository() = default;
     // ... 
-    virtual DATA insert(const DATA&) const = 0;
-    virtual DATA update(const DATA&) const = 0;
-    virtual void remove(const PKEY&) const = 0;
+    virtual DATA insert(const DATA&)  const = 0;
+    virtual DATA update(const DATA&)  const = 0;
+    virtual void remove(const PKEY&)  const = 0;
+    virtual DATA findOne(const PKEY&) const = 0;
 
 };
+
+template<class DATA>
+concept DataConcept = requires(DATA& data) {
+    data.getTableName();
+    data.toMap();
+};
+template<class DATA>
+requires DataConcept<DATA>
+class BasicRepository {
+public:
+    virtual std::string insertSql(const DATA& data)  const
+    {
+        puts("------ BasicRepository::insertSql()");
+        ptr_debug<const char*, const std::string&>("table is ", data.getTableName());
+        std::map<std::string, std::string> m = data.toMap();
+        for(auto p: m) {
+            std::cout << p.first << "\t: " << p.second << std::endl;
+        }
+        return std::string();
+    }
+    /**
+     * 単なるテンプレート型に過ぎない DATA をどのようにインスタンス化するのか。
+     * これが、できない限り私が望む理想的な Proxy にはならない。
+     * 今、思いつくのは、std::map を返却し、各リポジトリで DATA をインスタンス化する方法だが
+     * できれば、各リポジトリは宣言のみで、定義は行いたくない。
+     * あぁ、DataConcept で factory があればいいのかな。factory の仮引数に std::map があり、
+     * 各 DATA で std::map をもとに、自身を作る。そうすることで、リポジトリを汚染することは
+     * 回避できるかもしれない。
+    */
+    // virtual std::string updateSql(const DATA& data)  const = 0;
+    // virtual std::string removeSql(const DATA& data, const std::string& pkeyName)  const = 0;
+    // virtual std::string findOne(const DATA& data, const std::string& pkeyName) const = 0;
+};
+
+int test_BasicRepository() {
+    puts("=== test_BasicRepository");
+    try {
+        PersonData alice("Alice", "alice@loki.org", 12);
+        BasicRepository<PersonData> basicRepo;
+        basicRepo.insertSql(alice);
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        ptr_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
 
 /**
  * 実際に重要なのは、DATA のシグネチャやインタフェースにあると思う。
@@ -89,6 +191,39 @@ public:
  *                 .execute();
  * 
 */
+
+/**
+ * PersonData から動的に SQL を作る
+ * 
+ * Step 1 必要なデータの取得。
+*/
+
+int test_PersonData_Step_1() {
+    puts("=== test_PersonData_Step_1");
+    try {
+        PersonData alice("Alice", "alice@loki.org", 12);
+        PersonData cheshire("Cheshire", "cheshire@loki.org");
+        
+        std::map<std::string, std::string> map_a = alice.toMap();
+        puts("--- Alice Data");
+        ptr_debug<const char*, const std::string&>("table is ", alice.getTableName());
+        for(auto p: map_a) {
+            std::cout << p.first << "\t: " << p.second << std::endl;
+        }
+        puts("--- Cheshire Data");
+        ptr_debug<const char*, const std::string&>("table is ", cheshire.getTableName());
+        std::map<std::string, std::string> map_c = cheshire.toMap();
+        for(auto p: map_c) {
+            std::cout << p.first << "\t: " << p.second << std::endl;
+        }
+
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        ptr_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
 
 /**
  * C++ で map を使ってみる
@@ -122,7 +257,7 @@ int test_map() {
 
 int main(void) 
 {
-    puts("START Proxy パターン（であっているのかそれは分からない ===");
+    puts("START Proxy パターン（であっているのかそれは分からない） ===");
     if(0.01) {
         auto ret = 0;
         ptr_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_debug_error());
@@ -132,6 +267,10 @@ int main(void)
         auto ret = 0;
         ptr_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_map());
         assert(ret == 0);
+        ptr_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_PersonData_Step_1());
+        assert(ret == 0);
+        ptr_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_BasicRepository());
+        assert(ret == 0);
     }
-    puts("=== Proxy パターン（であっているのかそれは分からない） END");
+    puts("=== Proxy パターン（であっているのかそれは分からない）  END");
 }
