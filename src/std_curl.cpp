4ee46b267fd3e0e7c301171ff5f758c865721cd1
Jack <alienm14xr2@gmail.com>
2024-04-04 14:05:46 +0900

    C++ 課題 - curl。 - 個人的な興味。 - curl GET 実装とそのテスト。

--- src/std_curl.cpp
+++ src/std_curl.cpp
@@ -10,7 +10,7 @@
  * dpkg -L libcurl4
  * 
  * e.g. compile.
- * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror std_curl.cpp -o ../bin/main
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror -L/usr/lib/x86_64-linux-gnu/ std_curl.cpp -lcurl -o ../bin/main
 */
 #include <iostream>
 #include <cassert>
@@ -45,12 +45,59 @@ int test_debug_and_error() {
     }
 }
 
+size_t curl_write_func(char* cp, size_t size, size_t nmemb, std::string* stream) 
+{
+    size_t realSize = size * nmemb;
+    stream->append(cp, realSize);
+    return realSize;
+}
+
+std::string curl_get(const char* url) 
+{
+  CURL *curl;
+  CURLcode res = CURLE_UNSUPPORTED_PROTOCOL;
+  curl = curl_easy_init();
+  std::string chunk;
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_func);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &chunk);
+    curl_easy_setopt(curl, CURLOPT_PROXY, "");
+    res = curl_easy_perform(curl);
+    ptr_lambda_debug<const char*, const decltype(res)&>("res is ", res);
+    curl_easy_cleanup(curl);
+  }
+  if(res != CURLE_OK) {
+    std::cout << "ERROR: curl error." << std::endl;
+    std::exit(1);
+  }
+  return chunk;
+}
+
+int test_curl_get() {
+    puts("=== test_curl_get");
+    try {
+        char url[] = "http://www.loki.org/";
+        std::string res = curl_get(url);
+        ptr_lambda_debug<const char*, const std::string&>("res is ", res);
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        ptr_print_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
+    }
+}
+
 int main(void) {
     puts("START 課題 curl ===");
     if(0.01) {
         auto ret = 0;
         ptr_lambda_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_debug_and_error());
         assert(ret == 1);
+    }
+    if(1.00) {
+        auto ret = 0;
+        ptr_lambda_debug<const char*, const decltype(ret)&>("Play and Result ... ", ret = test_curl_get());
+        assert(ret == 0);
     }
     puts("===   課題 curl END");
     return 0;
