37f95fdddb3b481e187af5cae2eda72862dd44ba
Jack <alienm14xr2@gmail.com>
2025-12-02 19:31:18 +0900

    C++ Type Erasure - コンセプトとモデル、データの分離。 - 具体的なデータとモデルの実装の自由度が高い。

--- inc/Repository.hpp
+++ inc/Repository.hpp
@@ -15,6 +15,50 @@ public:
     virtual std::optional<Data> findById(const ID&) const = 0;
 };
 
+class CudConcept
+{
+public:
+    virtual ~CudConcept() = default;
+    virtual uint64_t insert() const = 0;
+    virtual void update() const = 0;
+    virtual void remove() const = 0;
+    virtual std::unique_ptr<CudConcept> clone() const = 0;
+};
+
+
+template <class Data, class Model>
+concept ModelNeeds = requires(Data& d, Model& m) {
+    m.insert(d);
+    m.update(d);
+    m.remove(d);
+};
+template<class Data, class Model>
+requires ModelNeeds<Data, Model>
+class CudModel final : public CudConcept
+{
+private:
+    Data data;  // ここにデータを持ちたくない。不変にもできない場合は困る。
+    Model model;
+public:
+    CudModel(Data _data, Model _model): data{std::move(_data)}, model{std::move(_model)}
+    {}
+    virtual uint64_t insert() const
+    {
+        return model.insert(data);
+    }
+    virtual void update() const
+    {
+        model.update(data);
+    }
+    virtual void remove() const
+    {
+        model.remove(data);
+    }
+    std::unique_ptr<CudConcept> clone() const override {
+        return std::make_unique<CudModel>(*this);
+    }
+};
+
 }   // namespace tmp
 
 #endif
